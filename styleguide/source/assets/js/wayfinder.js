b02fe8b46613415232388e4a86044a36ece613b1
Neil Gallagher <neil.gallagher@ama-assn.org>
2018-03-21 12:01:33 -0500

    EWL-4721: Adds conditional logic to show wayfinder based on either a css class is present or ama cookie

--- styleguide/source/assets/js/wayfinder.js
+++ styleguide/source/assets/js/wayfinder.js
@@ -14,12 +14,12 @@
         // Read wayfinder cookies set from ama-assn domains
         $.cookie.json = true;
         var ama_wayfinder_cookie = $.cookie('ama_wayfinder_cookie');
-        if (typeof ama_wayfinder_cookie !== 'undefined') {
-          $('.ama__wayfinder--referrer a').show().css('display', 'flex');
+        if (typeof ama_wayfinder_cookie !== 'undefined' || $('.referred').length > 0) {
+          $('.ama__wayfinder--referrer a').fadeIn().css('display', 'flex');
           $('.ama__wayfinder--referrer a').attr("href", ama_wayfinder_cookie[1]);
           $('.ama__wayfinder--referrer a').text(ama_wayfinder_cookie[0]);
         } else {
-          $('.ama_wayfinder_referrer--link-back a').hide();
+          $('.ama_wayfinder_referrer--link-back').fadeOut();
         }
       })(jQuery);
     }
