fb8d2b69fbea8c4de177d0affc46a37a8c24e2f5
nstriedinger <striedinger@palantir.net>
2018-03-30 11:57:45 -0400

    fix Twig functionality

---
+++ styleguide/source/_twig-components/functions/setattributes.function.php
@@ -0,0 +1,14 @@
+<?php
+
+$function = new Twig_SimpleFunction('setAttributes', function ($className, $attributes) {
+
+   if (method_exists($attributes,'addClass')) {
+     $attributes->addClass($className);
+   }
+   else {
+     $attributes = 'class=' . $className;
+   }
+  return $attributes;
+});
+
+?>
