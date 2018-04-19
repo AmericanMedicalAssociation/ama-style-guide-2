a3c0d9a9f5ddc7a2155590ae72e6bb21f51ea988
Lauren Burroughs <burroughs@palantir.net>
2018-04-19 17:32:40 -0500

    Update 2018-04-19T22:31:49.819Z

--- assets/js/wayfinder.js
+++ assets/js/wayfinder.js
@@ -11,15 +11,17 @@
   Drupal.behaviors.wayfinder = {
     attach: function (context, settings) {
       (function ($) {
-        // Read wayfinder cookies set from ama-assn domains
-        $.cookie.json = true;
-        var ama_wayfinder_cookie = $.cookie('ama_wayfinder_cookie');
-        if (typeof ama_wayfinder_cookie !== 'undefined' || $('.referred').length > 0) {
-          $('.ama__wayfinder--referrer a').fadeIn().css('display', 'flex');
-          $('.ama__wayfinder--referrer a').attr("href", ama_wayfinder_cookie[1]);
-          $('.ama__wayfinder--referrer a').text(ama_wayfinder_cookie[0]);
-        } else {
-          $('.ama_wayfinder_referrer--link-back').fadeOut();
+        if($.cookie('ama_wayfinder_cookie')) {
+          $.cookie.json = true;
+          // Read wayfinder cookies set from ama-assn domains
+          var ama_wayfinder_cookie = $.cookie('ama_wayfinder_cookie');
+          if (typeof ama_wayfinder_cookie !== 'undefined' || $('.referred').length > 0) {
+            $('.ama__wayfinder--referrer a').fadeIn().css('display', 'flex');
+            $('.ama__wayfinder--referrer a').attr("href", ama_wayfinder_cookie[1]);
+            $('.ama__wayfinder--referrer a').text(ama_wayfinder_cookie[0]);
+          } else {
+            $('.ama_wayfinder_referrer--link-back').fadeOut();
+          }
         }
       })(jQuery);
     }
