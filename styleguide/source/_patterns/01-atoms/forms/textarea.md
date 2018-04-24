996e57db93f3f6520e9c5c2c974f1a7027d0f389
Neil Gallagher <neil.gallagher@ama-assn.org>
2018-04-24 14:03:31 -0500

    EWL-4909: Add textarea with character count

---
+++ styleguide/source/_patterns/01-atoms/forms/textarea.md
@@ -0,0 +1,14 @@
+### Description
+This is the standard textarea pattern
+
+
+### Variables
+~~~
+{
+  "textarea": {
+    "label": type: string / optional
+    "name": type: string / optional
+    "placeholder": type: string / optional
+  }
+}
+~~~
--- styleguide/source/assets/js/form-items.js
+++ styleguide/source/assets/js/form-items.js
@@ -8,7 +8,23 @@
     attach: function (context, settings) {
       (function ($) {
         $(document).ready(function(){
-
+          // Textarea word count
+          function count_remaining_character() {
+            var max_length = 150;
+            var character_entered = $('.textarea').val().length;
+            var character_remaining = max_length - character_entered;
+            $('.character-count').html(character_remaining);
+            if(max_length < character_entered) {
+              $('.textarea').addClass('error');
+              $('.character-count').addClass('error');
+            } else {
+              $('.textarea').removeClass('error');
+              $('.character-count').removeClass('error');
+            }
+          }
+          $('.textarea').keyup(function() {
+            count_remaining_character();
+          });
         });
       })(jQuery);
     }
