519152b9f86bd2fa154664d5ed31f10b2cc34e41
Chelsie Johnston <chelsie.summer@gmail.com>
2018-04-26 18:08:07 -0500

    [EWL-4936] Start to add the resource link behavior.

--- styleguide/source/assets/js/resource-tabs.js
+++ styleguide/source/assets/js/resource-tabs.js
@@ -2,11 +2,45 @@ $('.ama__resource-tabs__nav li').click(function(){
   showTab($(this));
 });
 
-function showTab($tab) {
-  console.log($tab);
-  var dataTab = $tab.attr('data-tab');
-  console.log(dataTab);
+function showTab(tab) {
+  if ($.type(tab) === 'object') {
+    var sectionName = tab.attr('data-tab');
+    $tab = tab;
+  } else {
+    var sectionName = tab;
+
+  }
+
+  console.log('showing ' + sectionName);
   $('.--is-active').removeClass('--is-active');
-  $('.ama__resource-tabs__content section#' + dataTab).addClass('--is-active');
+  $('.ama__resource-tabs__content section#' + sectionName).addClass('--is-active');
   $tab.addClass('--is-active');
+}
+
+$('.ama__page--resource__resource-link').click(function(){
+  sectionName = getSectionName($(this));
+  findTab(sectionName);
+  console.log('show ' + $sectionName);
+
+});
+
+// Returns the section name of the clicked tab nav item.
+function getSectionName($link) {
+
+}
+
+// Returns a DOM object that corresponds with the nav item associated
+// with the clicked resource link.
+function findTab($resourceLink) {
+  console.log($resourceLink);
+  var resource = $resourceLink.attr('data-resource');
+  var resourceName = resource.substr(0, resource.indexOf('-'));
+  console.log(resourceName);
+  $('.ama__resource-tabs__nav li').each(function(){
+    var dataTab = $(this).attr('data-tab');
+    if(dataTab.indexOf(resourceName) >= 0) {
+      console.log('found tab' + dataTab);
+      return dataTab;
+    }
+  })
 }
