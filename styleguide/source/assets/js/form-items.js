5db78a4d8f6e5c1d24b71143c7c27b0cb4aa7454
Neil Gallagher <neil.gallagher@ama-assn.org>
2018-04-26 15:47:10 -0500

    Updates range atom to use numeric values and updates handle value when user drags

--- styleguide/source/assets/js/form-items.js
+++ styleguide/source/assets/js/form-items.js
@@ -12,17 +12,28 @@
 
           // Range Field
           var legend = $('.ama__range-field__legend');
+          var handle = $( "#currentValue" );
 
           $(".ama__range-field").slider({
             animate: true,
             range: "min",
-            value: 50,
-            min: 10,
+            value: 1,
+            min: 1,
             max: 100,
             step: 1,
+            create: function(){
+              var handle = jQuery(this).find('.ui-slider-handle');
+              var bubble = jQuery('<div class="valuebox"></div>');
+              handle.append(bubble);
+            },
+            slide: function(evt, ui) {
+              ui.handle.childNodes[0].innerHTML = ui.value;
+            }
           }).append(legend);
         });
       })(jQuery);
     }
   };
 })(jQuery, Drupal);
+
+
