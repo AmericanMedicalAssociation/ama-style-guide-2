4ae66f6ae6774ae3df2e0065daf120f0dd6417cc
Lauren Burroughs <burroughs@palantir.net>
2018-05-08 15:55:54 -0500

    Update 2018-05-08T20:55:34.636Z

--- assets/js/form-items.js
+++ assets/js/form-items.js
@@ -1,120 +1,69 @@
 /**
  * @file
- * Interactions for form validation.
- * Reference http://codepen.io/anon/pen/GHKJj
- *
- * JavaScript should be made compatible with libraries other than jQuery by
- * wrapping it with an "anonymous closure". See:
- * - https://drupal.org/node/1446420
- * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ * Form fields masking
  */
 
 (function ($, Drupal) {
-  Drupal.behaviors.formitems = {
+  Drupal.behaviors.formItems = {
     attach: function (context, settings) {
       (function ($) {
+        $(document).ready(function(){
+          $('[type=checkbox]').checkboxradio();
+          $('[type=radio]').checkboxradio().buttonset().find('label').css('width', '19.4%');
+          $('.ama__select-menu__select').selectmenu();
 
-        //////////////////////////////////
-        // Validate form
-        //////////////////////////////////
+          $('.multiselect').multiselect();
 
-        // Validate on page load
-        $('.simple-step-form, .multi-step-form').validate({
-          rules: {
-            tel: {
-              number: true
+          $('.ama__tooltip').tooltip({
+            tooltipClass: "ama__tooltip-bubble"
+          });
+
+          function count_remaining_character() {
+            var max_length = 150;
+            var character_entered = $('.textarea').val().length;
+            var character_remaining = max_length - character_entered;
+            $('.character-count').html(character_remaining);
+            if (max_length < character_entered) {
+              $('.textarea').addClass('error');
+              $('.character-count').addClass('error');
+            } else {
+              $('.textarea').removeClass('error');
+              $('.character-count').removeClass('error');
             }
-          },
-          ignore: ':hidden'
-        });
-
-        // Multi step form next link
-        $('.step-form-next').click(function() {
-
-          // Fieldset variables
-          var current_fs = $(this).closest('fieldset');
-          var next_fs = $(this).closest('fieldset').next('fieldset');
-          var prev_fs = $(this).closest('fieldset').prevAll('fieldset');
-
-          // If current fieldset is valid go to next fieldset
-          if($('.multi-step-form').valid()) {
-            current_fs.removeClass('active').addClass('complete');
-            next_fs.addClass('active');
-            prev_fs.addClass('complete');
           }
+
+          $('.textarea').keyup(function() {
+            count_remaining_character();
+          });
+
+          // Range Field
+          var legend = $('.ama__range-field__legend');
+          var handle = $( "#currentValue" );
+
+          $(".ama__range-field").slider({
+            animate: true,
+            range: 'min',
+            value: 1,
+            min: 2000,
+            max: 5000,
+            step: 1,
+            create: function(){
+              var handle = jQuery(this).find('.ui-slider-handle');
+              var bubble = jQuery('<div class="ama__range-field__valuebox"></div>');
+              handle.append(bubble);
+            },
+            slide: function(evt, ui) {
+              ui.handle.childNodes[0].innerHTML = '$' + ui.value;
+            }
+          }).append(legend);
+
+          // Form accordion
+          $( ".tablist" ).accordion({
+            header: ".ama__form-steps__step",
+            heightStyle: "content"
+          });
+
         });
-
-        // Step anchor links
-        $('.step-form-anchor').click(function() {
-
-          // Fieldset variable
-          var current_fs = $(this).closest('fieldset');
-
-          // If current fieldset has class do this
-          if(current_fs.hasClass('complete')) {
-            current_fs.removeClass('complete').addClass('active');
-            current_fs.siblings('fieldset').removeClass('active');
-          }
-        });
-
-
-        //////////////////////////////////
-        // Initiate form icons
-        //////////////////////////////////
-
-        // Create variable for all selectors needed for icon validator
-        var iconSelector = $('.form-item input[type="text"], .form-item input[type="email"], .form-item input[type="url"], .form-item input[type="date"], .form-item input[type="month"], .form-item input[type="time"], .form-item input[type="datetime"], .form-item input[type="datetime-local"], .form-item input[type="week"], .form-item input[type="number"], .form-item input[type="search"], .form-item input[type="tel"], .form-item input[type="color"], .form-item select, .form-item textarea, .form-item-radio label.error, .form-item-radio-label_group label:last-child, .form-item-checkbox label.error');
-
-        // Create form item icon
-        $(iconSelector).after('<span class="form-item_icon"></span>');
-
-
-        //////////////////////////////////
-        // Initiate label show/hide
-        //////////////////////////////////
-        var onClass = 'on';
-        var showClass = 'show';
-
-        $('input, textarea, select')
-        .bind('checkval', function()
-        {
-          var label = $(this).prev('label');
-
-          if (this.value !== '')
-          label.addClass(showClass);
-
-          else
-          label.removeClass(showClass);
-        })
-        .on('keyup', function()
-        {
-          $(this).trigger('checkval');
-        })
-        .on('focus', function()
-        {
-          $(this).prev('label').addClass(onClass);
-        })
-        .on('blur', function()
-        {
-          $(this).prev('label').removeClass(onClass);
-        })
-        .trigger('checkval');
-
-        //////////////////////////////////
-        // Show / Hide password text
-        //////////////////////////////////
-
-        $('.form-item_password').click(function() {
-          $(this).toggleClass('show');
-
-          if($(this).hasClass('show')) {
-            $(this).siblings('input').attr('type', 'text');
-          }
-          else {
-            $(this).siblings('input').attr('type', 'password');
-          }
-        });
-
       })(jQuery);
     }
   };
