9a6ef791834c7b97a308d6fb7354d252cf6aa3a5
Chelsie Johnston <chelsie.summer@gmail.com>
2018-06-11 13:57:20 -0500

    [EWL-5265] Add dropdown after convo with Toph.

--- styleguide/source/assets/js/subcategory-exploration.js
+++ styleguide/source/assets/js/subcategory-exploration.js
@@ -1,7 +1,7 @@
-$(document).click(function() {
-  var lastItem = $(this).find($('li:visible:last'));
-  console.log(lastItem);
-});
+// $(document).click(function() {
+//   var lastItem = $(this).find($('li:visible:last'));
+//   console.log(lastItem);
+// });
 
 // <li class="ama__subcategory-exploration__show-more">
 //   More
