053e9bc9495d75db820b2b6d46e31d33eb292e7e
Chelsie Johnston <chelsie.summer@gmail.com>
2018-06-11 13:10:32 -0500

    [EWL-5625] Attempt to add the expand/more behavior.

---
+++ styleguide/source/assets/js/subcategory-exploration.js
@@ -0,0 +1,8 @@
+$(document).click(function() {
+  var lastItem = $(this).find($('li:visible:last'));
+  console.log(lastItem);
+});
+
+// <li class="ama__subcategory-exploration__show-more">
+//   More
+//   </li>
