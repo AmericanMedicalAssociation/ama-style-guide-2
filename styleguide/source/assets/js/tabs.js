30454c21295162a2c1bca999ea6873298ab10533
Beatrice <beatricewms@gmail.com>
2018-06-25 16:40:35 -0500

    Switching resource navigation functionality to JqueryUI

--- styleguide/source/assets/js/tabs.js
+++ styleguide/source/assets/js/tabs.js
@@ -1,35 +1,5 @@
 /*=======Resource Tabs=======*/
 
-$('*[data-resource]').click(function(){
-  // Figure out which tab to display.
-  $tab = findTab($(this));
-  // Show the tab.
-  showTab($tab);
-});
-
-// function showTab() - shows and hides the tab.
-function showTab($tab) {
-  $('.--is-active').removeClass('--is-active');
-  // Show the section.
-  $('.ama__resource-tabs__content section#' + $tab.attr('id')).addClass('--is-active');
-  // Put an active state on the tab.
-  $('.ama__resource-tabs__nav li[data-resource="' + $tab.attr('id') + '"]').addClass('--is-active');
-}
-
-// function findTab() - return object that is the section to be displayed.
-function findTab($obj) {
-  // Get the ID for the section to display.
-  var resourceData =_getSectionID($obj);
-  // Find out which tab the sectionID corresponds to.
-  $('.ama__resource-tabs section').each(function() {
-    var attrID = $(this).attr('id');
-    if(attrID.indexOf(resourceData['sectionID']) >= 0) {
-      $tab = $(this);
-    }
-  });
-  return $tab;
-}
-
 // function _getSectionID() - return array containing the specific resource to show and the section element's ID attribute.
 function _getSectionID($obj) {
   // Parse out the section ID from the clicked object.
@@ -52,6 +22,10 @@ function _getSectionID($obj) {
           return false;
         }
       });
+    $(".ama__tabs > ul a").click(function(e){
+        e.preventDefault()
+        return false;
+    });
 
     //Simulate click event on actual simpleTabs tab from mobile drop down.
     $('.ama__tabs-navigation--mobile select').on( "selectmenuchange", function( event, ui ) {
