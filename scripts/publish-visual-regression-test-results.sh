4cc072606866d7dcbdb06800849c8a126f0f9208
Daniel Montgomery <damontgomery@gmail.com>
2018-07-02 19:20:31 -0500

    [EWL-4538] Publish artifact to github as a branch.
    On a failure, a report is published to `visual-regression-testing-artifact/[pull-request-id]` for pull requests and `visual-regression-testing-artifact/[branch-id]` for branches.
    
    A github token and a github branch name must be provided as environmental variables to Travis.
    
    `GITHUB_TOKEN_VRT`, see https://github.com/settings/tokens
    `GITHUB_TOKEN_REPO`, example `@github.com/AmericanMedicalAssociation/ama-style-guide-2.git`
    
    Travis has been configured with the new token and caching of the vendor directories `vendor` and `node_modules` has been added to speed up the automated testing.

--- scripts/publish-visual-regression-test-results.sh
+++ scripts/publish-visual-regression-test-results.sh
@@ -8,11 +8,14 @@ else
   BRANCH_ID="${TRAVIS_BRANCH}"
 fi
 
-deploy_branch="test-results-${BRANCH_ID}"
+deploy_branch="visual-regression-testing-artifact/${BRANCH_ID}"
+
+# Define the github repo using a token and environmental variables.
+GITHUB_REPO="https://${GITHUB_TOKEN_VRT}${GITHUB_TOKEN_REPO}"
 
 # Fail if we still don't know where the repo is.
-echo "Deploying to the SG2 repo at '${REPO}'."
-if [ -z "${REPO}" ] ; then
+echo "Deploying to the SG2 repo at '${GITHUB_REPO}'."
+if [ -z "${GITHUB_REPO}" ] ; then
     echo "Cannot deploy without a repo."
     exit 1
 fi
@@ -20,19 +23,21 @@ fi
 echo "Setting up git"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git clone --bare "${REPO}" artifacts/current/.git
 
-# Don't do anything else if it didn't work.
-if git remote -v | grep -q 'github'
-then
-    echo "clone failed"
-    exit 1
-fi
+mkdir visual-regression-testing-artifact
+cd visual-regression-testing-artifact
+
+git init
+
+git remote add origin ${GITHUB_REPO}
 
 echo "Committing to ${deploy_branch}."
 git checkout -b "${deploy_branch}"
-git add backstop_data/
+# Copy artifacts
+cp -R ../backstop_data/* .
+git add .
 git commit --message "Travis build: ${TRAVIS_BUILD_ID}"
 
 echo "Deploying to ${deploy_branch}."
-git push origin ${deploy_branch}
+# Force push in case there were artifacts for this PR / branch before.
+git push -f origin ${deploy_branch}
