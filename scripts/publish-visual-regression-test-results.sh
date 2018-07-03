82429cab8b336998081ef652496e04494963ba5b
Daniel Montgomery <damontgomery@gmail.com>
2018-07-03 09:21:06 -0500

    [EWL-4538] Refactor script variables.
    Use lowercase for script variables, uppercase for environmental variables.
    The existing `GITHUB_TOKEN` environmental variable has the right permissions to create branches.

--- scripts/publish-visual-regression-test-results.sh
+++ scripts/publish-visual-regression-test-results.sh
@@ -3,19 +3,19 @@ set -e # Exit with nonzero exit code if anything fails
 
 # Setup a deploy branch
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  BRANCH_ID="${TRAVIS_PULL_REQUEST}"
+  git_branch_id="${TRAVIS_PULL_REQUEST}"
 else
-  BRANCH_ID="${TRAVIS_BRANCH}"
+  git_branch_id="${TRAVIS_BRANCH}"
 fi
 
-deploy_branch="visual-regression-testing-artifact/${BRANCH_ID}"
+git_deploy_branch="visual-regression-testing-artifact/${git_branch_id}"
 
 # Define the github repo using a token and environmental variables.
-GITHUB_REPO="https://${GITHUB_TOKEN_VRT}${GITHUB_TOKEN_REPO}"
+github_remote="https://${GITHUB_TOKEN}${GITHUB_TOKEN_REPO}"
 
 # Fail if we still don't know where the repo is.
-echo "Deploying to the SG2 repo at '${GITHUB_REPO}'."
-if [ -z "${GITHUB_REPO}" ] ; then
+echo "Deploying to the SG2 repo at '${github_remote}'."
+if [ -z "${github_remote}" ] ; then
     echo "Cannot deploy without a repo."
     exit 1
 fi
@@ -29,15 +29,15 @@ cd visual-regression-testing-artifact
 
 git init
 
-git remote add origin ${GITHUB_REPO}
+git remote add origin ${github_remote}
 
-echo "Committing to ${deploy_branch}."
-git checkout -b "${deploy_branch}"
+echo "Committing to ${git_deploy_branch}."
+git checkout -b "${git_deploy_branch}"
 # Copy artifacts
 cp -R ../backstop_data/* .
 git add .
 git commit --message "Travis build: ${TRAVIS_BUILD_ID}"
 
-echo "Deploying to ${deploy_branch}."
+echo "Deploying to ${git_deploy_branch}."
 # Force push in case there were artifacts for this PR / branch before.
-git push -f origin ${deploy_branch}
+git push -f origin ${git_deploy_branch}
