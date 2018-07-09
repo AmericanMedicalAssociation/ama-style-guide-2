44edf6f01122459d47af8f3ea3a14de4075ddec1
Beatrice <beatricewms@gmail.com>
2018-07-09 15:18:38 -0500

    Update link icon classes, link icon styles and ama__tabs related scripts.

--- styleguide/source/assets/js/tabs.js
+++ styleguide/source/assets/js/tabs.js
@@ -1,31 +1,24 @@
-/*=======Resource Tabs=======*/
-
-// function _getSectionID() - return array containing the specific resource to show and the section element's ID attribute.
-function _getSectionID($obj) {
-  // Parse out the section ID from the clicked object.
-  var resourceData = [];
-  resourceData['resourceID'] = $obj.attr('data-resource');
-  resourceData['sectionID'] = resourceData['resourceID']
-    .substr(0, resourceData['resourceID'].indexOf('-'));
-  if (!resourceData['sectionID']) {
-    resourceData['sectionID'] = resourceData['resourceID'];
-  }
-  return resourceData;
-}
 /*====== jQuery UI tabs ======*/
 
 
 (function($) {
-    $( ".ama__tabs" ).tabs();
+    var defaultActiveTab = 0
     var viewportWidth = window.innerWidth;
-    if (viewportWidth < 600) {
-        $(".ama__resource-tabs").tabs();
-    }else{
-        $(".ama__resource-tabs").tabs({
-            active: 1
-        });
+    if (viewportWidth > 600 && $('.ama__resource-tabs').length > 0) {
+        defaultActiveTab = 1;
     }
-    $(".ama__tabs > ul a").click(function(e){
+    $(".ama__tabs").tabs({
+        active: defaultActiveTab,
+        create: function() {
+            var widget = $(this).data('ui-tabs');
+            $(window).on('hashchange', function() {
+                widget.option('active', widget._getIndex(location.hash));
+            });
+        }
+    });
+
+    // Prevent jump onclick
+    $('.ama__resource-tabs__nav a, .ama__tabs-navigation a').on('click', function(e){
         e.preventDefault();
         return false;
     });
