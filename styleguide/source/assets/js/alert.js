a9406e3ca95fca235b0a2e05adf257e942d5c8d8
Lauren Burroughs <burroughs@palantir.net>
2018-07-13 10:52:41 -0500

    refs EWL-5444: clean up alert.js

--- styleguide/source/assets/js/alert.js
+++ styleguide/source/assets/js/alert.js
@@ -7,52 +7,26 @@
  * - https://drupal.org/node/1446420
  * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
  */
-(function ($) {
-  $(document).ready(function() {
-    console.log('doc ready');
+ (function ($, Drupal) {
+   Drupal.behaviors.alert = {
+     attach: function (context, settings) {
+       Cookies.get('ama__alert--hide');
+       var alertCookie = $.Cookies('ama__alert--hide');
+       (function ($) {
+         // If the 'hide cookie is not set we show the alert
+         if (!alertCookie) {
+           $('.ama__alert').fadeIn("slow");
+         }
 
-    // If the 'hide cookie is not set we show the alert
-    if (!readCookie('hideAlert')) {
-      console.log('no cookie');
-      $('.ama__alert').fadeIn("slow");
-    }
-
-    // Add the event that closes the popup and sets the cookie that tells us to
-    // not show it again until one day has passed.
-    $('.ama__alert__close').click(function() {
-      $('.ama__alert').fadeOut();
-      console.log('cookie created');
-      createCookie('hideAlert', true, 1);
-      return false;
-    });
-
-  });
-
-  // ---
-  // And some generic cookie logic
-  // ---
-  function createCookie(name,value,days) {
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime()+(days*24*60*60*1000));
-      var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
-  }
-
-  function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-  }
-
-  function eraseCookie(name) {
-    createCookie(name,"",-1);
-  }
-})(jQuery);
+         // Add the event that closes the popup and sets the cookie that tells us to
+         // not show it again until one day has passed.
+         $('.ama__alert__close').click(function() {
+           $('.ama__alert').fadeOut();
+           // set the cookie
+           Cookies.set('ama__alert--hide', '1', { expires: 1});
+           return false;
+         });
+       })(jQuery);
+     }
+   };
+ })(jQuery, Drupal);
