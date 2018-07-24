0c00701de27c06d537f7c387e8d733094619ee06
Neil Gallagher <neil.gallagher@ama-assn.org>
2018-07-24 15:44:07 -0500

    EWL-5591 global search

---
+++ styleguide/source/assets/js/global-search.js
@@ -0,0 +1,28 @@
+(function ($, Drupal) {
+  Drupal.behaviors.ama_search = {
+    attach: function (context, settings) {
+      var searchInput = $('.ama__global-search .ama__search__field--in-body input');
+      var mobileSearch = $('.ama__global-search--mobile');
+
+      function switchSearch() {
+        if (searchInput.css('display') === 'none' && mobileSearch.css('display') === 'none') {
+          mobileSearch.fadeIn();
+        }
+        else if (searchInput.css('display') === 'block') {
+          $(this).submit();
+        }
+        else {
+          mobileSearch.fadeOut();
+        }
+      }
+
+      $('.ama__search__field__button').click(function () {
+        switchSearch();
+      });
+
+      $( window ).resize(function() {
+        switchSearch();
+      });
+    }
+  };
+})(jQuery, Drupal);
