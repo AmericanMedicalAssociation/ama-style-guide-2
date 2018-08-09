adbd208e81fa74c26644f1f87becb3040d0341a5
Beatrice <beatricewms@gmail.com>
2018-08-09 16:35:28 -0500

    Update 2018-08-09T21:33:39.132Z

--- assets/js/subcategory-exploration.js
+++
@@ -1,4 +0,0 @@
-$('.ama__subcategory-exploration__show-more').click(function() {
-  $('.ama__subcategory-exploration__list').toggleClass('ama__subcategory-exploration__list--expanded');
-  $(this).toggleClass('ama__subcategory-exploration__show-more--expanded');
-});
