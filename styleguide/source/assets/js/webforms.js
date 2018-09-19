8b8a1d2ec49664994eb048987db264143fedd79a
Neil Gallagher <neil.gallagher@ama-assn.org>
2018-09-19 11:15:16 -0500

    Adds validation to selects using jQuery UI

--- styleguide/source/assets/js/webforms.js
+++ styleguide/source/assets/js/webforms.js
@@ -4,13 +4,21 @@
       (function ($) {
         // On webform submit check to see if all inputs are valid
         $('.webform-submission-form').validate({
+          ignore: [],
+          errorPlacement: function(error, element) {
+            if (element.attr("type") == "checkbox") {
+              error.insertAfter(element.parent().siblings().last());
+            } else if (element.is("select")) {
+              error.insertAfter(element.next())
+            } else error.insertAfter(element);
+          },
           invalidHandler: function(form, validator) {
             var errors = validator.numberOfInvalids();
             if (errors) {
               $('.progress-marker').addClass('error')
             }
           }
-        })
+        });
 
         // Check to see if inputs are valid
         $('.webform-submission-form input').keyup(function() {
@@ -20,6 +28,17 @@
             $('.progress-marker').addClass('error')
           }
         });
+        
+        // Add validation to select dropdown menus using jQuery UI
+        $('.webform-submission-form select').selectmenu({
+          style: 'dropdown',
+          transferClasses: true,
+          width: null,
+          change: function() {
+            $(".webform-submission-form").validate().element(this);
+          }
+        });
+
       })(jQuery);
     }
   };
