d455bb3b68df31db5b6bd03095549fbe4c2f76e9
Beatrice <beatricewms@gmail.com>
2018-10-03 00:42:15 -0500

    Add smooth scroll to logic for resource page tab link refs.

--- styleguide/source/assets/js/tabs.js
+++ styleguide/source/assets/js/tabs.js
@@ -6,6 +6,7 @@
     attach: function (context, settings) {
       var defaultActiveTab = 0;
       var viewportWidth = window.innerWidth;
+      var lastHash = null;
       if (viewportWidth >= 600 && $('.ama__resource-tabs').length > 0) {
         defaultActiveTab = 1;
       }
@@ -21,6 +22,11 @@
           var widget = $(this).data('ui-tabs');
           $(window).on('hashchange', function () {
             widget.option('active', widget._getIndex(location.hash));
+            lastHash = location.hash;
+
+            // Scroll to top of ui tabs navigation
+            smoothScroll($(widget.element), $(widget.active[0]));
+            return false;
           });
         }
       });
@@ -36,11 +42,36 @@
         window.scroll({top: windowScrollY});
       });
 
+      // Scroll to when there is no has change on resource page tab links
+      $('.ama____page--resources__page-content a[href^="#"]').on('click',function (e) {
+        if(lastHash == window.location.hash){
+          var hash = window.location.hash;
+          smoothScroll($('.ama__resource-tabs__nav'),$('.ama__resource-tabs__nav a[href="'+hash+'"]'));
+        }
+      });
+
       //Simulate click event on actual simpleTabs tab from mobile drop down.
       $('.ama__tabs-navigation--mobile select').on("selectmenuchange", function (event, ui) {
         var selectedValue = ui.item.value;
         $('a[href="#' + selectedValue + '"]').click();
       });
+
+      /*
+       * This function animates the browser scroll action with attention to keyboard only accessibility concerns
+       *
+       * @param {jQuery Object] $tabNav
+       * @param {jQuery Object] $target
+       */
+      function smoothScroll($tabNav, $target) {
+        var navCoords = $tabNav[0].getBoundingClientRect();
+        $('html,body').animate({
+          scrollTop: window.scrollY + navCoords.top
+        }, 850, function () {
+          // update focus for keyboard only navigation
+          $target.attr('tabindex', '-1');
+          $target.focus();
+        });
+      }
     }
   };
 })(jQuery, Drupal);
