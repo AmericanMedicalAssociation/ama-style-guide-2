9a16225737af0adc65cce191433b2078daa81a8b
Beatrice <beatricewms@gmail.com>
2018-10-10 16:08:28 -0500

    Update tab functions to simplify code and correct functionality in D8.

--- styleguide/source/assets/js/tabs.js
+++ styleguide/source/assets/js/tabs.js
@@ -6,7 +6,6 @@
     attach: function (context, settings) {
       var defaultActiveTab = 0;
       var viewportWidth = window.innerWidth;
-      var lastHash = null;
       if (viewportWidth >= 600 && $('.ama__resource-tabs').length > 0) {
         defaultActiveTab = 1;
       }
@@ -15,40 +14,11 @@
         active: defaultActiveTab
       });
 
-      //Resource page specific tags
-      $(".ama__resource-tabs").tabs({
-        active: defaultActiveTab,
-        create: function () {
-          var widget = $(this).data('ui-tabs');
-          $(window).on('hashchange', function () {
-            widget.option('active', widget._getIndex(location.hash));
-            lastHash = location.hash;
-
-            // Scroll to top of ui tabs navigation
-            smoothScroll($(widget.element), $(widget.active[0]));
-            return false;
-          });
-        }
-      });
-
       // Prevent jump onclick
       $('.ui-tabs-anchor').on('click', function (e) {
-        // Store window y location so we can restore after changing the hash
-        // which would otherwise cause the window to jump down.
-        var windowScrollY = window.scrollY;
-        // Update window hash location, and restore to previous y-position.
-        // Use currentTarget because target is sometimes the icon div.
-        window.location.hash = e.currentTarget.hash;
-        window.scroll({top: windowScrollY});
+        return false;
       });
 
-      // Scroll to when there is no has change on resource page tab links
-      $('.ama____page--resources__page-content a[href^="#"]').on('click', function (e) {
-        if (lastHash == window.location.hash) {
-          var hash = window.location.hash;
-          smoothScroll($('.ama__resource-tabs__nav'), $('.ama__resource-tabs__nav a[href="' + hash + '"]'));
-        }
-      });
 
       //Simulate click event on actual simpleTabs tab from mobile drop down.
       $('.ama__tabs-navigation--mobile select').on("selectmenuchange", function (event, ui) {
@@ -56,11 +26,20 @@
         $('a[href="#' + selectedValue + '"]').click();
       });
 
+      // When clicking an inline resource page link referencing a tab, open referenced tab.
+      $('.ama__resource-link--inline, .ama__page--resource__resource-link').on('click', function (e) {
+        var $tabs = $('.ama__resource-tabs');
+        var $clickedObj = $(this);
+        var linkHash = this.getAttribute("href");
+        switchTabs($tabs, linkHash);
+        return false;
+      });
+
       /*
        * This function animates the browser scroll action with attention to keyboard only accessibility concerns
        *
-       * @param {jQuery Object] $tabNav
-       * @param {jQuery Object] $target
+       * @param {jQuery Object} $tabNav
+       * @param {jQuery Object} $target
        */
       function smoothScroll($tabNav, $target) {
         var navCoords = $tabNav[0].getBoundingClientRect();
@@ -71,6 +50,23 @@
           $target.attr('tabindex', '-1');
           $target.focus();
         });
+      }
+
+      /*
+       * This function opens referenced tabs from inline links
+       *
+       * @param {jQuery Object} $tabObj The element which has the .tab() function attached.
+       * @param {string} linkHash
+       */
+      function switchTabs($tabObj, linkHash) {
+        var widget = $tabObj.data('ui-tabs');
+        var tabIndex = widget._getIndex(linkHash);
+
+        $tabObj.tabs({
+          active: tabIndex
+        });
+        // Scroll to top of ui tabs navigation
+        smoothScroll($tabObj, $(widget.active[0]));
       }
     }
   };
