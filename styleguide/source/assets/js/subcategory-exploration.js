41c480b856ea620d4029b4b19b9655f50aebbe87
Neil Gallagher <neil.gallagher@ama-assn.org>
2018-10-16 12:37:33 -0500

    Changes var to have a $ in front of per Drupal standards

--- styleguide/source/assets/js/subcategory-exploration.js
+++ styleguide/source/assets/js/subcategory-exploration.js
@@ -11,32 +11,32 @@
   Drupal.behaviors.subcategoriesExploration = {
     attach: function(context, settings) {
 
-      var subcategoryListContainer = $('.ama__subcategory-exploration__list');
-      var subcategoryList  = $('.ama__subcategory-exploration__list ul');
-      var subcategoryListExpander = $('.ama__subcategory-exploration__show-more');
-      var subcategoryListContainerHeight = subcategoryListContainer.outerHeight();
-      var subcategoryListLinkText = $('.ama__subcategory-exploration__text');
+      var $subcategoryListContainer = $('.ama__subcategory-exploration__list');
+      var $subcategoryList  = $('.ama__subcategory-exploration__list ul');
+      var $subcategoryListExpander = $('.ama__subcategory-exploration__show-more');
+      var $subcategoryListContainerHeight = $subcategoryListContainer.outerHeight();
+      var $subcategoryListLinkText = $('.ama__subcategory-exploration__text');
 
       // If the unordered list height is greater than the parent container then show the show more link
-      if (subcategoryList.height() > subcategoryListContainerHeight) {
-        subcategoryListExpander.show();
+      if ($subcategoryList.height() > $subcategoryListContainerHeight) {
+        $subcategoryListExpander.show();
       }
 
       // Drupal compels me to unbind clicks otherwise double clicks occur
-      subcategoryListExpander.unbind('click').click(function(e){
+      $subcategoryListExpander.unbind('click').click(function(e){
         e.stopPropagation();
         e.preventDefault();
 
         // Checks to see if the container has been expand or not by comparing initial height to current height
-        if(subcategoryListContainer.height() > subcategoryListContainerHeight) {
-          subcategoryListContainer.removeClass('ama__subcategory-exploration__list--expanded');
+        if($subcategoryListContainer.height() > $subcategoryListContainerHeight) {
+          $subcategoryListContainer.removeClass('ama__subcategory-exploration__list--expanded');
           $(this).removeClass('ama__subcategory-exploration__show-more--expanded');
-          subcategoryListLinkText.text('View all subcategories');
+          $subcategoryListLinkText.text('View all subcategories');
         }
         else {
-          subcategoryListContainer.addClass('ama__subcategory-exploration__list--expanded');
+          $subcategoryListContainer.addClass('ama__subcategory-exploration__list--expanded');
           $(this).addClass('ama__subcategory-exploration__show-more--expanded');
-          subcategoryListLinkText.text('View fewer subcategories');
+          $subcategoryListLinkText.text('View fewer subcategories');
         }
       });
     }
