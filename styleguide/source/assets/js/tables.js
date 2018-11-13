a68524bd324fab365d7ba779809fb136b258e63e
Neil Gallagher <neil.gallagher@ama-assn.org>
2018-11-13 09:33:39 -0600

    Turns table into a list if columns exceed 5.

--- styleguide/source/assets/js/tables.js
+++ styleguide/source/assets/js/tables.js
@@ -10,11 +10,38 @@
 (function ($, Drupal) {
   Drupal.behaviors.responsiveTables = {
     attach: function(context, settings) {
-      $("th", context).each(function () {
+
+      var $tableCols = 0;
+
+      $('table tbody tr').each(function(){
+
+        var $currCount = 0;
+
+        $(this).children('td').each(function(){
+
+          $currCount++;
+
+          var $colSpan = $(this).attr('colspan');
+
+          if ($colSpan > 0) {
+            $currCount = $currCount + ($colSpan - 1);
+          }
+
+          if ($currCount > $tableCols) {
+            $tableCols = $currCount;
+          }
+        }); // next td
+
+        if($currCount > 5) {
+         $(this).parent().parent().addClass('table-list');
+        }
+      });
+
+      $('th', context).each(function () {
         var eq = $(this).index();
         var child = eq + 1;
         var label = $(this).text();
-        $("td:nth-child(" + child + ")").append("&nbsp;").attr("data-title", label).addClass("responsive");
+        $("td:nth-child(" + child + ")").append('&nbsp;').attr('data-title', label).addClass('responsive');
       });
     }
   };
