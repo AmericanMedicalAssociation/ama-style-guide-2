672d600c1a1d295655bbf0a1ce4ca5a9e3727e03
Neil Gallagher <neil.gallagher@ama-assn.org>
2019-03-07 11:58:15 -0600

    removes sticky social icons
    This was added by accident

--- styleguide/source/assets/js/nav.js
+++ styleguide/source/assets/js/nav.js
@@ -23,59 +23,6 @@
         } else {
           $('.ama__main-navigation ').sticky({ zIndex: 501 });
         }
-
-        function moveSocialSharePosition(layoutPositionUpdate){
-          if($('.ama__masthead__content__share').length && $(window).width() > 768) {
-
-            var $layoutPosition = 0;
-            var $socialIcons = $('.ama__masthead__content__share');
-            var $article = $('.layout__region').length  ? $('.layout__region') : $('article');
-
-            if(typeof layoutPositionUpdate !== 'undefined'){
-              $layoutPosition = layoutPositionUpdate;
-            } else {
-              $layoutPosition = $article.offset().left - 130;
-            }
-
-            if ($layoutPosition < 0) {
-              $layoutPosition = -400;
-            }
-
-            $socialIcons.sticky({ wrapperClassName: 'ama__masthead__content__share-wrapper', zIndex: 501 });
-
-            $socialIcons.on('sticky-start', function () {
-              $('.ama__social-share').addClass('ama__social-share--fixed').css('left', $layoutPosition).hide().fadeTo('slow', 1);
-            });
-
-            $socialIcons.on('sticky-update', function () {
-              $('.ama__social-share').addClass('ama__social-share--fixed').css('left', $layoutPosition).hide().fadeTo('slow', 1);
-            });
-
-            $socialIcons.on('sticky-end', function () {
-              $('.ama__social-share--fixed').removeClass('ama__social-share--fixed');
-            });
-          }
-        }
-
-        // Initialize getSocialShare()
-        moveSocialSharePosition();
-
-        //Checks the layout position of article on window resize and moves the social icons accordingly
-        $( window ).resize(function() {
-          var $layoutPositionUpdate = $('.layout__region').offset().left - 130;
-          var $socialIcons = $('.ama__social-share');
-
-          if ($layoutPositionUpdate < 0) {
-            $layoutPositionUpdate = -100;
-          }
-
-          // Update social icon position after window resize
-          $socialIcons.css('left', $layoutPositionUpdate);
-
-          moveSocialSharePosition($layoutPositionUpdate);
-
-        });
-
       });
 
       $('.ama__ribbon__dropdown').each(function () {
