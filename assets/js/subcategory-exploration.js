62a14ccf6e5f5f209b5fb13d4c4345f88d9426b1
Christine Harder <christine.harder@ama-assn.org>
2019-07-24 14:15:17 -0500

    Update 2019-07-24T19:13:50.653Z

--- assets/js/subcategory-exploration.js
+++ assets/js/subcategory-exploration.js
@@ -16,11 +16,38 @@
       var $subcategoryListExpander = $('.ama__subcategory-exploration__show-more');
       var $subcategoryListContainerHeight = $subcategoryListContainer.outerHeight() + 3;
       var $subcategoryListLinkText = $('.ama__subcategory-exploration__text');
+      var $initialWindowWidth = $(window).width();
 
-      // If the unordered list outerHeight is greater than the parent container then show the show more link
-      if ($subcategoryList.outerHeight() > $subcategoryListContainerHeight) {
-        $subcategoryListExpander.show();
-      }
+      // Determine when to show link based on window size.
+      $(document).ready(function () {
+        function showHideMoreLink () {
+          // Set intial window width to 1024 pixel.
+          // @todo: Check if this is an issue to set 1024 width limit.
+          if ($initialWindowWidth <= 1024) {
+            // If the unordered list outerHeight is greater than the parent container then show the show more link,
+            // hide otherwise.
+            if ($subcategoryList.outerHeight() > $subcategoryListContainerHeight) {
+              $subcategoryListExpander.show();
+            }
+            if ($subcategoryList.outerHeight() < $subcategoryListContainerHeight) {
+              $subcategoryListExpander.hide();
+            }
+          }
+          if ($initialWindowWidth !== $(window).width()) {
+            // If the unordered list outerHeight is greater than the parent container then show the show more link
+            if ($subcategoryList.outerHeight() > $subcategoryListContainerHeight) {
+              $subcategoryListExpander.show();
+
+            } else {
+              $subcategoryListExpander.hide();
+            }
+          }
+        }
+
+        showHideMoreLink();
+          $(window).resize(showHideMoreLink);
+      });
+
 
       // Drupal compels me to unbind clicks otherwise double clicks occur
       $subcategoryListExpander.unbind('click').click(function(e){
