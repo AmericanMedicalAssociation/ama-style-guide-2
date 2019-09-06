1c925bdeaccb2da4d85f733ceae6888c824c5a1b
Neil Gallagher <neil.gallagher@ama-assn.org>
2019-09-06 12:31:35 -0500

    Update 2019-09-06T17:30:56.707Z

---
+++ assets/js/listicle.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Listicle Clases.
+ *
+ * Handling classes to build listicle properly outside ckeditor.
+ */
+(function ($, Drupal) {
+  Drupal.behaviors.listicle = {
+    attach: function(context, settings) {
+      if ($('.listicle', context).length) {
+        $('.listicle').each(function () {
+          $(this).children().each(function (idx, e) {
+            $(e).addClass('listicle__item');
+            $(e).children('ol').each(function (idx, f) {
+              $(f).addClass('listicle__item-sub');
+              $(f).children('li').addClass('listicle__item-sub-item');
+            });
+          });
+        });
+      }
+    }
+  };
+})(jQuery, Drupal);
