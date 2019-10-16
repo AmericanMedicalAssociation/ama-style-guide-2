3c5653dc279068d3582cab7cc9f7c8816ed60ae2
dr-zee <e-blast@joshthorne.com>
2019-10-16 17:26:38 -0500

    EWL-7563 switched to newer node version and updated package.json. began process of updating gulpfile to gulp 4 with mizxed results.

--- styleguide/gulpfile.js
+++ styleguide/gulpfile.js
@@ -16,7 +16,6 @@ var gulp      = require('gulp'),
   tagversion  = require('gulp-tag-version'),
   uglify      = require('gulp-uglify'),
   ghPages     = require('gulp-gh-pages'),
-  runSequence = require('run-sequence'),
   glob        = require('glob'),
   sourcemaps  = require('gulp-sourcemaps'),
   prefix      = require('gulp-autoprefixer'),
@@ -26,14 +25,13 @@ var gulp      = require('gulp'),
   gutil       = require('gulp-util'),
   pWaitFor    = require('p-wait-for'),
   pathExists  = require('path-exists'),
-  plumber     = require('gulp-plumber');
+  plumber     = require('gulp-plumber')
 
 // Config
-var config = require('./build.config.json');
-
+var config = require('./build.config.json')
 
 // Trigger
-var production;
+var production
 // Task: Clean:before
 // Description: Removing assets files before running other tasks
 gulp.task('clean:before', function () {
@@ -43,14 +41,79 @@ gulp.task('clean:before', function () {
     .pipe(clean({
       force: true
     }))
-});
+})
+
+// Copy twig files from source
+/* copy files */
+gulp.task('copyTwigFiles', function () {
+  return gulp.src(config.twigsource.files)
+    .pipe(plumber())
+    .pipe(gulp.dest(config.twigsource.dest))
+})
+
+// Create reference screenshots from `gh-pages` and `referenceUrl`
+gulp.task('reference', function () {
+  return gulp.src('')
+    .pipe(shell(['backstop reference']))
+})
+
+// Run backstop to run tests
+gulp.task('backstop', function () {
+  return gulp.src('')
+    .pipe(shell(['backstop test']))
+    .on('error', function () {
+      // For now, we do not want to stop when tests fail since we are
+      // using them only for reporting. Swap `0` for `1` in the code below to
+      // trigger a failure.
+      process.exit(0)
+    })
+})
+
+// Task: Watch files
+gulp.task('watch', function (done) {
+  // Watch Pattern Lab files
+  gulp.watch(
+    config.patternlab.files,
+    gulp.series('patternlab', 'default')
+  )
+
+  // Watch scripts
+  gulp.watch(
+    config.scripts.files,
+    ['scripts']
+  )
+
+  // Watch media
+  gulp.watch(
+    config.images.files,
+    ['images']
+  )
+
+  // Watch sass
+  gulp.watch(
+    config.scss.watch,
+    ['sass']
+  )
+
+  // Watch fonts
+  gulp.watch(
+    config.fonts.files,
+    ['fonts']
+  )
+
+  gulp.watch(
+    config.twigsource.files,
+    ['cleanTwig']
+  )
+  done()
+})
 
 // Task: Clean:publish
 // Description: Removing temp dir from git deploy
 gulp.task('clean:publish', function () {
-  return gulp.src( '.publish' )
+  return gulp.src('.publish')
     .pipe(clean({ force: true }))
-});
+})
 
 // Task: Handle scripts
 gulp.task('scripts', function () {
@@ -61,7 +124,7 @@ gulp.task('scripts', function () {
     .pipe(sourcemaps.init())
     .pipe(concat('styleguide-custom.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.scripts.dest));
+    .pipe(gulp.dest(config.scripts.dest))
 
   // Package up everything for use by Pattern Lab
   return gulp.src(config.scripts.files)
@@ -74,13 +137,13 @@ gulp.task('scripts', function () {
     // also 'production-ready' js file even though we don't use it yet
     .pipe(rename('app.min.js'))
     .pipe(uglify())
-    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()) })
     .pipe(gulp.dest(config.scripts.dest))
     .pipe(browserSync.reload({
-      stream:true,
-      notify:false
-    }));
-});
+      stream: true,
+      notify: false
+    }))
+})
 
 // Task: Handle fonts
 gulp.task('fonts', function () {
@@ -90,10 +153,10 @@ gulp.task('fonts', function () {
       config.fonts.dest
     ))
     .pipe(browserSync.reload({
-      stream:true,
-      notify:false
-    }));
-});
+      stream: true,
+      notify: false
+    }))
+})
 
 // Task: Handle media
 gulp.task('images', function () {
@@ -104,23 +167,97 @@ gulp.task('images', function () {
       config.images.dest
     ))
     .pipe(browserSync.reload({
-      stream:true,
-      notify:false
-    }));
-});
-
+      stream: true,
+      notify: false
+    }))
+})
 
 // Task: Handle icons
 // We have to do this in a few steps until
 // https://github.com/filamentgroup/gulpicon/issues/1 is resolved
-gulp.task('minifyIcons', function() {
+gulp.task('minifyIcons', function () {
   return gulp.src(config.icons.files)
     .pipe(plumber())
     .pipe(svgmin())
-    .pipe(gulp.dest(config.icons.min));
-});
+    .pipe(gulp.dest(config.icons.min))
+})
 
-gulp.task('sass', ['scss-lint'], function () {
+// Task: Sass Linting
+// Description: lint sass files
+gulp.task('scss-lint', function () {
+  return gulp.src(config.scss.files)
+    .pipe(stylelint({
+      reporters: [
+        { formatter: 'string', console: true }
+      ]
+    }))
+})
+
+gulp.task('set-master', function () {
+  // Change the deploy branch
+  gutil.log('Setting branch to master.')
+  config.deployment.branch = 'master'
+})
+
+// Task: patternlab
+// Description: Build static Pattern Lab files via PHP script
+gulp.task('patternlab', function () {
+  return gulp.src('', { read: false })
+    .pipe(plumber())
+    .pipe(shell([
+      'php core/console --generate'
+    ]))
+    .pipe(browserSync.reload({
+      stream: true,
+      notify: false
+    }))
+})
+
+// Task: styleguide
+// Description: Copy Styleguide-Folder from core/ to public
+gulp.task('styleguide', function () {
+  return gulp.src(config.patternlab.styleguide.files)
+    .pipe(plumber())
+    .pipe(gulp.dest(config.patternlab.styleguide.dest))
+})
+
+// task: BrowserSync
+// Description: Run BrowserSync server with disabled ghost mode
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: config.root
+    },
+    notify: false,
+    ghostMode: true,
+    open: 'local'
+  })
+})
+
+gulp.task('exit', function () {
+  process.exit()
+})
+
+// copy files settings
+var svg2twig = {
+  base: config.icons.base,
+  src: config.icons.files,
+  dest: './source/_patterns/01-atoms/media/icons/'
+}
+
+/* copy files */
+gulp.task('svg2twig', function (done) {
+  done()
+  return gulp.src(svg2twig.src, { base: svg2twig.base })
+    .pipe(plumber())
+    .pipe(rename({
+      extname: '.twig'
+    }))
+    .pipe(gulp.dest(svg2twig.dest))
+})
+
+gulp.task('sass', gulp.series('scss-lint', function (done) {
+  done()
   return gulp.src(config.scss.files)
     .pipe(plumber())
     .pipe(sourcemaps.init())
@@ -137,238 +274,64 @@ gulp.task('sass', ['scss-lint'], function () {
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(config.scss.dest))
     .pipe(browserSync.reload({
-      stream:true,
-      notify:false
-    }));
-});
-
-// Task: patternlab
-// Description: Build static Pattern Lab files via PHP script
-gulp.task('patternlab', function () {
-  return gulp.src('', {read: false})
-    .pipe(plumber())
-    .pipe(shell([
-      'php core/console --generate'
-    ]))
-    .pipe(browserSync.reload({
-      stream:true,
-      notify:false
-    }));
-});
-
-// Task: styleguide
-// Description: Copy Styleguide-Folder from core/ to public
-gulp.task('styleguide', function() {
-  return gulp.src(config.patternlab.styleguide.files)
-    .pipe(plumber())
-    .pipe(gulp.dest(config.patternlab.styleguide.dest));
-});
-
-// task: BrowserSync
-// Description: Run BrowserSync server with disabled ghost mode
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-      baseDir: config.root
-    },
-    notify: false,
-    ghostMode: true,
-    open: "local"
-  });
-});
-
-gulp.task('exit', function() {
-  process.exit();
-});
-
-// Task: Sass Linting
-// Description: lint sass files
-gulp.task('scss-lint', function() {
-  return gulp.src(config.scss.files)
-    .pipe(stylelint({
-      reporters: [
-        {formatter: 'string', console: true}
-      ]
-    }));
-});
-
-
-// copy files settings
-var svg2twig = {
-  base: config.icons.base,
-  src: config.icons.files,
-  dest: "./source/_patterns/01-atoms/media/icons/"
-};
-
-/* copy files */
-gulp.task("svg2twig", function() {
-  return gulp.src(svg2twig.src, { base: svg2twig.base })
-    .pipe(plumber())
-    .pipe(rename({
-      extname: ".twig"
+      stream: true,
+      notify: false
     }))
-    .pipe(gulp.dest(svg2twig.dest))
-});
+}))
 
-gulp.task('cleanTwig', ['clean:before'], function (callback) {
-  runSequence(
-    'patternlab',
-    'copyTwigFiles',
-    callback
-  );
-});
-
-// Copy twig files from source
-/* copy files */
-gulp.task("copyTwigFiles", function() {
-  return gulp.src(config.twigsource.files)
-    .pipe(plumber())
-    .pipe(gulp.dest(config.twigsource.dest))
-});
-
-// Create reference screenshots from `gh-pages` and `referenceUrl`
-gulp.task( 'reference', function () {
-  return gulp.src('')
-    .pipe(shell(['backstop reference']))
-});
-
-// Run backstop to run tests
-gulp.task( 'backstop', function () {
-  return gulp.src('')
-    .pipe(shell(['backstop test']))
-    .on('error', function () {
-      // For now, we do not want to stop when tests fail since we are 
-      // using them only for reporting. Swap `0` for `1` in the code below to 
-      // trigger a failure.
-      process.exit(0)
-    });
-});
-
-// Task: Watch files
-gulp.task('watch', function () {
-
-  // Watch Pattern Lab files
-  gulp.watch(
-    config.patternlab.files,
-    ['patternlab', 'default']
-  );
-
-  // Watch scripts
-  gulp.watch(
-    config.scripts.files,
-    ['scripts']
-  );
-
-  // Watch media
-  gulp.watch(
-    config.images.files,
-    ['images']
-  );
-
-  // Watch sass
-  gulp.watch(
-    config.scss.watch,
-    ['sass']
-  );
-
-  // Watch fonts
-  gulp.watch(
-    config.fonts.files,
-    ['fonts']
-  );
-
-  gulp.watch(
-    config.twigsource.files,
-    ['cleanTwig']
-  );
-});
+gulp.task('cleanTwig', gulp.series('clean:before', 'patternlab', 'copyTwigFiles', function (done) {
+  done()
+}))
 
 // Task: Default
 // Description: Build all stuff of the project once
-gulp.task('default', ['clean:before'], function (callback) {
-  production = false;
-
-  // We need to re-run sass last to make sure the latest styles.css gets loaded
-  runSequence(
-    ['fonts', 'images'],
-    'patternlab',
-    'styleguide',
-    'copyTwigFiles',
-    'svg2twig',
-    'sass',
-    'scripts',
-    callback
-  );
-});
-
-// gulp.task('default', runSequence(['scripts', 'fonts', 'images', 'sass', 'patternlab', 'styleguide']));
+gulp.task('default', gulp.series('clean:before', 'fonts', 'images', 'patternlab', 'styleguide', 'copyTwigFiles', 'svg2twig', 'sass', 'scripts', function (done) {
+  done()
+  production = false
+}))
 
 // Task: Start your production-process
 // Description: Type 'gulp' in the terminal
-gulp.task('serve', function () {
-  production = false;
-
-  runSequence(
-    'default',
-    'browser-sync',
-    'watch'
-  );
-});
+gulp.task('serve', gulp.series('default', 'browser-sync', 'watch', function (done) {
+  done()
+  production = false
+}))
 
 // Task: Run visual regression tests
 // Description: Type 'gulp test' in the terminal
 // Create reference screenshots from `gh-pages`, build site, run backstop and stop browserSync
-gulp.task('test', function () {
-  production = false;
-  runSequence(
-    'reference',
-    'default',
-    'browser-sync',
-    'backstop'
-  );
-});
+gulp.task('test', gulp.series('reference', 'default', 'browser-sync', 'backstop', function (done) {
+  done()
+  production = false
+}))
 
 // Task: Publish static content
 // Description: Publish static content using rsync shell command
-gulp.task('publish', ['clean:publish'], function () {
+gulp.task('publish', gulp.series('clean:publish', function (done) {
+  done()
   return gulp.src(config.deployment.local.path)
-    .pipe(ghPages({ branch: config.deployment.branch}));
-});
+    .pipe(ghPages({
+      branch: config.deployment.branch
+    }))
+}))
 
 // Task: Deploy to GitHub pages
 // Description: Build the public code and deploy it to GitHub pages
-gulp.task('deploy', function () {
-  // make sure to use the gulp from node_modules and not a different version
-  runSequence = require('run-sequence').use(gulp);
-  // run default to build the code and then publish it GitHub pages
-  runSequence('default', 'publish');
-});
+gulp.task('deploy', gulp.series('default', 'publish', function (done) {
+  done()
+}))
 
 // Task: Deploy to dev-assets branch
 // Description: Build the public code and deploy it to be consumed by Drupal
-gulp.task('drupal-deploy', function () {
-  // make sure to use the gulp from node_modules and not a different version
-  runSequence = require('run-sequence').use(gulp);
+gulp.task('drupal-deploy', gulp.series('default', 'copyTwigFiles', 'publish', function (done) {
+  done()
   // Change the deploy branch
-  config.deployment.branch = "dev-assets";
-  // run default to build the code and then publish it to our branch
-  runSequence('default', 'copyTwigFiles', 'publish');
-});
-
-gulp.task('set-master', function (callback) {
-  // Change the deploy branch
-  gutil.log('Setting branch to master.');
-  config.deployment.branch = "master";
-  callback();
-})
+  config.deployment.branch = 'dev-assets'
+}))
 
 // Task: Release the code
 // Description: Release runs deploy to build to gh-pages,
 // pushes the same code to master, then tags master.
-gulp.task('release', function (callback) {
-  // make sure to use the gulp from node_modules and not a different version
-  runSequence = require('run-sequence').use(gulp);
-  // Build the style guide, publish to gh-pages, set the branch to master,
-  // publish to master, then tag master.
-  runSequence('default', 'publish', 'set-master', callback);
-});
+gulp.task('release', gulp.series('default', 'publish', 'set-master', function (done) {
+  done()
+}))
