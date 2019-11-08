930c7c31f21ef36cda27ee1b41a83062b7b078a3
Josh <gsdproductions@yahoo.com>
2019-11-08 02:11:33 -0600

    EWL-7550 Added functions to give list items clearfix if they contain images, and tuned the width and margins of images displayed in the list.

--- styleguide/source/assets/js/listicle.js
+++ styleguide/source/assets/js/listicle.js
@@ -18,16 +18,25 @@
           });
         });
       }
+      //if there is an inline promo on a page with a listicle, determine if the list is close enough beneath the promo in the dom to assume it will be floated next to it. I chose within 5 siblings.
       if($('.ama__promo--inline ~ .listicle')) {
         var length = $('.ama__promo--inline').first().nextUntil('.listicle').addBack().length;
         if (length <= 5) {
           $('.ama__promo--inline').addClass('listicle-margin');
         }
       }
-      if($('.listicle__item > img')) {
-        $('.listicle__item > img').each(function() {
-
-          $(this).parent().css("background-color", "red")
+      //if the listicle item contains an image, put a clearfix div on the item so if it has a trailing image, the next item won't wrap on it.
+      //Also, determine it the image is almost 100% of the list width. if it is, add a class to remove the left margin and make the image 100% width. I chose 80%.
+      if($('.listicle__item img')) {
+        $('.listicle img').each(function () {
+          var fixedThis = this
+          var width = $('.listicle').width()
+          var imageWidth = $(this).width()
+          var clearfix = '<div class="clearfix"></div>'
+          $(this).closest('.listicle__item').once().append(clearfix)
+          if (imageWidth >= width*.8) {
+            $(this).addClass('no-margin')
+          }
         })
       }
     }
