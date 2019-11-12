92981892a494bb61f4c4caceca8786d19d734d03
dr-zee <e-blast@joshthorne.com>
2019-11-12 17:50:36 -0600

    Update 2019-11-12T23:48:54.885Z

--- assets/js/listicle.js
+++ assets/js/listicle.js
@@ -18,6 +18,29 @@
           });
         });
       }
+      //if there is an inline promo on a page with a listicle, determine if the list is close enough beneath the promo in the dom to assume it will be floated next to it. I chose within 5 siblings.
+      if($('.ama__promo--inline ~ .listicle')) {
+        var length = $('.ama__promo--inline').first().nextUntil('.listicle').addBack().length;
+        if (length <= 5) {
+          $('.ama__promo--inline').addClass('listicle-margin');
+        }
+      }
+      //if the listicle item contains an image, put a clearfix div on the item so if it has a trailing image, the next item won't wrap on it.
+      //Also, determine it the image is almost 100% of the list width. if it is, add a class to remove the left margin and make the image 100% width. I chose 80%.
+      if($('.listicle__item img')) {
+        $('.listicle__item img').each(function () {
+          var width = $(this).closest('.listicle__item').width()
+          console.log(width)
+          var imageWidth = $(this).width()
+          console.log(imageWidth)
+          var clearfix = '<div class="clearfix"></div>'
+          $(this).closest('.listicle__item').once().append(clearfix)
+          if (imageWidth >= width*.7) {
+            $(this).addClass('no-margin')
+            $(this).closest ('figure').addClass('no-margin')
+          }
+        })
+      }
     }
   };
 })(jQuery, Drupal);
