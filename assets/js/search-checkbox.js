f3bc36b4fbd1744d10a87b1121359bc10db267b1
Christine Harder <christine.harder@ama-assn.org>
2019-11-20 16:29:16 -0600

    Update 2019-11-20T22:28:14.375Z

---
+++ assets/js/search-checkbox.js
@@ -0,0 +1,40 @@
+(function ($, Drupal) {
+  Drupal.behaviors.ama_search_checkbox = {
+    attach: function (context, settings) {
+
+      var $categorySearchInput = $('#search_category');
+      var $categorySearchList = $('.facets-widget-checkbox ul li');
+      var $clearSearchFilter = $('#appliedFiltersRemove');
+
+      // Filter list using jQuery filter
+      function filterList(searchBox, list) {
+        searchBox.keyup(function () {
+          var $regex = new RegExp(this.value, 'i');
+          list.hide().filter(function () {
+            return $regex.test($.trim($(this).text()));
+          }).show();
+        });
+      }
+
+      // Clear filter
+      function cleafFilterList(clearSearchFilter) {
+        clearSearchFilter.click(function (e) {
+          e.preventDefault();
+          $categorySearchInput.val('');
+          $categorySearchInput.trigger('keyup');
+
+          $('.facets-widget-checkbox ul li [type=checkbox]').each( function() {
+            $(this).prop("checked", false);
+            $('#block-exposedformacquia-searchpage').submit();
+          });
+        });
+      }
+
+      // Invoke filter list
+      filterList($categorySearchInput, $categorySearchList);
+
+      // Invoke clear filter
+      cleafFilterList($clearSearchFilter);
+    }
+  };
+})(jQuery, Drupal);
