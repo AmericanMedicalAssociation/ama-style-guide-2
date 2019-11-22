2c6df4f9a2de480829e2adc2b4af151b2f7605d7
Christine Harder <christine.harder@ama-assn.org>
2019-11-22 17:25:03 -0600

    Update 2019-11-22T23:21:10.698Z

---
+++ assets/js/alert.js
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * alert.
+ *
+ * JavaScript should be made compatible with libraries other than jQuery by
+ * wrapping it with an "anonymous closure". See:
+ * - https://drupal.org/node/1446420
+ * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ */
+ (function ($, Drupal) {
+   Drupal.behaviors.alert = {
+     attach: function (context, settings) {
+       $.cookie('ama__alert--hide');
+       var alertCookie = $.cookie('ama__alert--hide');
+       
+       (function ($) {
+         // If the 'hide cookie is not set we show the alert
+         if (alertCookie !== '1') {
+           $('.ama__alert__wrap').fadeIn("slow");
+         }
+
+         // Add the event that closes the popup and sets the cookie that tells us to
+         // not show it again until one day has passed.
+         $('.ama__alert__close').click(function() {
+           $('.ama__alert__wrap').fadeOut();
+           // set the cookie
+           $.cookie('ama__alert--hide', '1', { expires: 1});
+           return false;
+         });
+       })(jQuery);
+     }
+   };
+ })(jQuery, Drupal);
---
+++ assets/js/category-menu.js
@@ -0,0 +1,14 @@
+/**
+ * SmartMenus jQuery Plugin - v1.1.0 - September 17, 2017
+ * http://www.smartmenus.org/
+ *
+ * Copyright Vasil Dinkov, Vadikom Web Ltd.
+ * http://vadikom.com
+ *
+ * Licensed MIT
+ */
+
+
+jQuery('.ama_category_navigation_menu__group').smartmenus({
+  subIndicatorsPos: 'append'
+});
