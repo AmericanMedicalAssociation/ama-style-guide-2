d2f0d3f732a9ae25eb1e4002a5a83f8e801cbd8b
Christine Harder <christine.harder@ama-assn.org>
2020-06-11 20:51:10 -0500

    JS for the jump menu on the article pages

---
+++ styleguide/source/assets/js/toc.js
@@ -0,0 +1,20 @@
+(function ($, Drupal) {
+  Drupal.behaviors.ama_toc = {
+    attach: function (context, settings) {
+      $(document).ready(function() {
+        $('.toc a').bind('click', function(e) {
+            e.preventDefault(); // prevent hard jump, the default behavior
+
+            var target = $(this).attr("href"); // Set the target as variable
+
+            // perform animated scrolling by getting top-position of target-element and set it as scroll target
+            $('html, body').stop().animate({
+                scrollTop: $(target).offset().top - $('#main-content').offset().top + $('.workbench-tabs').height()
+            }, 600);
+
+            return false;
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);
