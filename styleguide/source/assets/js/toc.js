b1524ca3fde8e181683ef1ed3ccc6e791a523bd1
Christine Harder <christine.harder@ama-assn.org>
2020-06-23 11:38:05 -0500

    Edit scroll JS to work when workbench tabs are not rendered

--- styleguide/source/assets/js/toc.js
+++ styleguide/source/assets/js/toc.js
@@ -6,10 +6,9 @@
             e.preventDefault(); // prevent hard jump, the default behavior
 
             var target = $(this).attr("href"); // Set the target as variable
-
             // perform animated scrolling by getting top-position of target-element and set it as scroll target
             $('html, body').stop().animate({
-                scrollTop: $(target).offset().top - $('#main-content').offset().top + $('.workbench-tabs').height()
+                scrollTop: $(target).offset().top - $('#main-content').offset().top + ($('.workbench-tabs').height()?$('.workbench-tabs').height():0)
             }, 600);
 
             return false;
