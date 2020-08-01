070c36c89d2bcee79de142daf1dbec27b9e50c46
dr-zee <e-blast@joshthorne.com>
2020-08-01 01:04:35 -0500

    js and style work.

--- styleguide/source/assets/js/index-page.js
+++ styleguide/source/assets/js/index-page.js
@@ -2,20 +2,38 @@
   Drupal.behaviors.index = {
     attach: function (context, settings) {
 
-      $textLength = $('.fulltext p').html().length
+      var fullText = $('.fulltext').html()
+      var truncated = $('.truncated').html()
+      var desc = $('.desc-display')
+      var test = $('.ama__category-index .ama__layout--two-col-right--75-25__left')
+      var content = test.html()
+      var temp = $('.temp-div')
+      var tempHeight = ''
+      var truncHeight = ''
 
-      // if (length > 50) {
-      //   $('.fulltext p:last-child').once().append(' <span class="more visible"> ...Read More</span>');
-      // }
+      $('.desc-display', context).once('getHeight').each(function () {
+        test.html(content).append('<div class="temp-div">' + fullText + '</div>')
+        truncHeight = desc.height()
+        tempHeight = temp.height()
+        truncHeight = desc.height()
+        console.log(tempHeight)
+        desc.css('height', truncHeight + 28 + 'px')
+        temp.remove()
+      })
 
-      $('.more', context).on('click', function () {
-        $('.less').toggleClass('visible')
-        $('.more').toggleClass('visible')
+      desc.on('click', '.more', function () {
+        desc.addClass('full')
+        desc.removeClass('summary')
+        desc.css('height', tempHeight + 'px')
+        desc.html(fullText).append('<span class="less">Hide Content</span>')
       })
-      $('.less', context).on('click', function () {
-        $('.less').toggleClass('visible')
-        $('.more').toggleClass('visible')
+      desc.on('click', '.less', function () {
+        desc.removeClass('full')
+        desc.css('height', truncHeight + 'px')
+        desc.addClass('summary')
+        desc.html(truncated).append('<span class="more"> ...Read More</span>')
       })
+
     }
   }
 })(jQuery, Drupal)
