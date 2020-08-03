a45d2f3adc3ddf20f2307bd7a83487a4eaefa048
dr-zee <e-blast@joshthorne.com>
2020-08-03 15:14:16 -0500

    cleanup

--- styleguide/source/assets/js/index-page.js
+++ styleguide/source/assets/js/index-page.js
@@ -11,40 +11,46 @@
       var truncHeight = ''
       var moreHtml = '<span class="more"> ...Read More</span>'
       var lessHtml = '<span class="less">Hide Content</span>'
-      var resizeFull = ''
-      var resizeTrunc = ''
 
-      $('.desc-display', context).once('getHeight').each(function () {
-        truncHeight = trunc.height()
+      function getDimensions () {
         fullHeight = full.height() + 35
+        truncHeight = trunc.height()
+      }
+      // Animate the height of a dynamic height object? SIMPLE!
+      // What a fool you would be to not think of so elegant a solution.
+      // In the markup, there are hidden fulltextsummary divs.
+      // They are absolutely positioned in the markup to keep an accurate height.
+      // Set height on pageload using the hidden divs.
+      $('.desc-display', context).once('getHeight').each(function () {
+        getDimensions()
         desc.css('height', truncHeight + 'px')
       })
 
+      // Set the height again on window resize.
       $(window).on('resize', function () {
+        getDimensions()
         if (desc.hasClass('full')) {
-          resizeFull = full.height() + 35
-          desc.css('height', resizeFull + 'px')
+          desc.css('height', fullHeight + 'px')
         } else if (desc.hasClass('summary')) {
-          resizeTrunc = trunc.height()
-          desc.css('height', resizeTrunc + 'px')
+          desc.css('height', truncHeight + 'px')
         }
       })
 
+      // On click, set the height to trigger css transition.
       desc.on('click', '.more', function () {
-        truncHeight = trunc.height()
-        fullHeight = full.height() + 35
+        getDimensions()
         desc.css('height', fullHeight + 'px')
-        desc.addClass('full')
-        desc.removeClass('summary')
+        desc.addClass('full').removeClass('summary')
+        // Swap the truncated copy into the display div.
         desc.html(fullText).append(lessHtml)
       })
       desc.on('click', '.less', function () {
-        truncHeight = trunc.height()
-        fullHeight = full.height() + 35
+        getDimensions()
         desc.css('height', truncHeight + 'px')
-        desc.removeClass('full')
-        desc.addClass('summary')
+        desc.addClass('summary').removeClass('full')
+        // Swap the truncated copy into the display div.
         desc.html(truncated).append(moreHtml)
+        // Scroll to top.
         $('html, body').animate({ scrollTop: 0 }, 500)
       })
     }
