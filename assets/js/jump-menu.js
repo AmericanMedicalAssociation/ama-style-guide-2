c8281b78c009ba40291ffcd3ecd08cf25d90d68a
dr-zee <e-blast@joshthorne.com>
2021-01-22 17:19:59 -0600

    Update 2021-01-22T23:16:50.556Z

---
+++ assets/js/jump-menu.js
@@ -0,0 +1,9 @@
+(function ($, Drupal) {
+  Drupal.behaviors.jumpMenu = {
+    attach: function (context, settings) {
+      $('.ama__jump_menu').on('selectmenuchange', function () {
+        window.location = $(this).find(':selected').data('url');
+      });
+    }
+  };
+})(jQuery, Drupal);
