7022775726489261352ef8521404231ec53d44d3
dr-zee <e-blast@joshthorne.com>
2021-01-22 17:23:55 -0600

    Update 2021-01-22T23:20:39.060Z

---
+++ assets/js/tabs.js
@@ -0,0 +1,128 @@
+/*====== jQuery UI tabs ======*/
+
+
+(function ($, Drupal) {
+  Drupal.behaviors.ama_tabs = {
+    attach: function (context, settings) {
+      var defaultActiveTab = 0;
+      var viewportWidth = window.innerWidth;
+      if (viewportWidth >= 600 && $('.ama__resource-tabs').length > 0) {
+        defaultActiveTab = 1;
+      }
+
+      $(".ama__tabs, .ama__resource-tabs").tabs({
+        active: defaultActiveTab,
+        activate: removeHighlights
+      });
+
+      // Prevent jump onclick
+      $('.ui-tabs-anchor').on('click', function (e) {
+        e.preventDefault();
+        return false;
+      });
+
+
+      //Simulate click event on actual simpleTabs tab from mobile drop down.
+      $('.ama__tabs-navigation--mobile select').on("selectmenuchange", function (event, ui) {
+        var selectedValue = ui.item.value;
+        $('a[href="#' + selectedValue + '"]').click();
+      });
+
+      // When clicking an inline resource page link referencing a tab, open referenced tab.
+      $('.ama__resource-link--inline, .ama__page--resource__resource-link').on('click', function (e) {
+
+        e.preventDefault();
+
+        var $tabs = $('.ama__resource-tabs');
+        switchTabs($tabs, this);
+        // Stop bubbling and default actions
+        return false;
+      });
+
+      function removeHighlights() {
+        $('.ama_resource-header--highlight').removeClass('ama_resource-header--highlight');
+      }
+
+      /*
+       * This function animates the browser scroll action with attention to keyboard only accessibility concerns
+       *
+       * @param {jQuery Object} $tabNav
+       * @param {jQuery Object} $target
+       */
+      function smoothScroll($tabNav, tabHash, positionInTab) {
+        var scrollTarget = window.innerWidth >= 1200 ? '.ama__resource-tabs__content' : 'html,body';
+
+        // Remove previous highlights, if any
+        removeHighlights();
+
+        // Try to find target element offset, but default to zero
+        var scrollPosition = 0;
+        var $target;
+        if (positionInTab !== undefined) {
+          var tabElements = $(tabHash + ' .ama__resource-tabs__item');
+          if (tabElements.length) {
+            // If desired position is larger than the result set, use the last element
+            if (tabElements.length <= positionInTab) {
+              positionInTab = tabElements.length;
+            }
+            // Users are instructed to consider 1 as the first element
+            var target = tabElements[positionInTab - 1];
+            scrollPosition = target.offsetTop;
+            // Add highlight to target
+            $target = $(target).find('.ama_resource-header'); // save for use in animate() callback
+            $target.addClass('ama_resource-header--highlight');
+          }
+        } else {
+          $target = $(tabHash);
+        }
+        
+        $(scrollTarget).animate({
+          scrollTop: scrollPosition
+        }, 850, function () {
+          // Update focus for keyboard only navigation
+          $target.attr('tabindex', '-1').focus();
+        });
+
+        // Stop bubbling and default actions
+        return false;
+      }
+
+      /*
+       * This function opens referenced tabs from inline links
+       *
+       * @param {jQuery Object} $tabObj The element which has the .tab() function attached.
+       * @param {Element} link
+       */
+      function switchTabs($tabObj, link) {
+
+        var linkHash = link.getAttribute("href");
+        var widget = $tabObj.data('ui-tabs');
+
+        var tabHash, positionInTab;
+        var parts = linkHash.split('-');
+        tabHash = parts[0];
+        if (parts.length > 1) {
+          positionInTab = parts[1];
+        } else {
+          // If old link, try to determine position from link text
+          var matches = link.innerText.match(/([0-9]+)/g);
+          if (matches) {
+            positionInTab = matches.shift();
+          }
+        }
+
+        // Ensure correct tab is active
+        var tabIndex = widget._getIndex(tabHash);
+        $tabObj.tabs({
+          active: tabIndex
+        });
+
+        // Scroll to top of ui tabs navigation
+        smoothScroll($tabObj, tabHash, positionInTab);
+
+        // Stop bubbling and default actions
+        return false;
+      }
+    }
+  };
+})(jQuery, Drupal);
