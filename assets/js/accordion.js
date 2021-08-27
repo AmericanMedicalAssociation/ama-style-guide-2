e0f99d9b45f205f15bee4330429d2952aaed2d4f
Christine Harder <christine.harder@ama-assn.org>
2021-08-27 18:27:04 +0000

    Update 2021-08-27T18:23:07.277Z

---
+++ assets/js/accordion.js
@@ -0,0 +1,9 @@
+/*====== jQuery UI accordion ======*/
+
+(function($) {
+    $( ".ama__accordion" ).accordion({
+        heightStyle: "content",
+        collapsible: true,
+        active: false
+    });
+})(jQuery);
---
+++ assets/js/resource.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Sadly adds footer to left resource page column.
+ *
+ * JavaScript should be made compatible with libraries other than jQuery by
+ * wrapping it with an "anonymous closure". See:
+ * - https://drupal.org/node/1446420
+ * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ */
+(function ($, Drupal) {
+  Drupal.behaviors.resourcePageFooter = {
+    attach: function(context, settings) {
+
+      $(function() {
+        if ($('body.ama__resource-page').length) {
+          $('footer', context).clone().appendTo('.ama__layout--split__left').addClass('ama__footer ama__resource-page__desktop-footer');
+        }
+      });
+    }
+  };
+})(jQuery, Drupal);
