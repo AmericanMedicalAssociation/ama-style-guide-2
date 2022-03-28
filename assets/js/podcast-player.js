4af357ec0a35788d08a784babeddb366f5f92aea
Christine Harder <christine.harder@ama-assn.org>
2022-03-28 14:15:47 +0000

    Update 2022-03-28T14:11:41.194Z

--- assets/js/podcast-player.js
+++ assets/js/podcast-player.js
@@ -2,29 +2,9 @@
   Drupal.behaviors.ama_podcast = {
     attach: function (context, settings) {
       $(document).ready(function() {
-        //Check initial window with
-        playerPlacement();
-
         //Check number of links
         oddLinks();
-      
-        //Check window width on resize
-        $(window).resize(function() {
-          playerPlacement();
-        });
-
       });
-
-      function playerPlacement() {
-        var podcastImage = $('div.ama__podcast-player .ama__image');
-        var width = $(document).width();
-
-        if (width < 600) {
-          podcastImage.insertBefore('div.podcast-container');
-        } else if (width > 600) {
-          podcastImage.insertAfter('div.ama__podcast-player__episode_info');
-        }
-      }
 
       function oddLinks() {
         var count = $("ul.ama__podcast-player__links li").length;
