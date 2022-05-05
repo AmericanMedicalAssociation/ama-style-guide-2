1aea766d3baf3bed4da488edc8e7d23c37247b86
joshuathorne <e-blast@joshthorne.com>
2022-05-05 18:58:12 +0000

    Update 2022-05-05T18:57:00.236Z

---
+++ assets/js/alert.js
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * alert.
+ *
+ * JavaScript should be made compatible with libraries other than jQuery by
+ * wrapping it with an "anonymous closure". See:
+ * - https://drupal.org/node/1446420
+ * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ */
+ (function ($, Drupal) {
+   Drupal.behaviors.alert = {
+     attach: function (context, settings) {
+      var alert = $('.ama__alert__wrap').attr('id');
+       $.cookie('ama__alert--' + alert);
+       var alertCookie = $.cookie('ama__alert--' + alert);
+            
+       (function ($) {
+         // If the 'hide cookie is not set we show the alert
+         if (alertCookie !== '1') {
+           $('.ama__alert__wrap').css({
+             "transition": "opacity .15s",
+             "opacity": "1"
+            });
+         } else {
+           $('.ama__alert__wrap').css({
+            "display": "none"
+           });
+         }
+
+         // Add the event that closes the popup and sets the cookie that tells us to
+         // not show it again until one day has passed.
+         $('.ama__alert__close').click(function() {
+           $('.ama__alert__wrap').css({
+            "transition": "opacity 2s",
+             "opacity": "0",
+             "display": "none"
+            });
+           // set the cookie
+           $.cookie('ama__alert--' + alert, '1', { expires: 1});
+           return false;
+         });
+       })(jQuery);
+     }
+   };
+ })(jQuery, Drupal);
