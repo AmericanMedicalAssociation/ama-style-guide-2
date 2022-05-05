6d61f7586d4ada938b48f20e90ad1fff9cb3e17b
joshuathorne <e-blast@joshthorne.com>
2022-05-05 18:56:41 +0000

    Update 2022-05-05T18:55:29.008Z

---
+++ assets/js/main-navigation.js
@@ -0,0 +1,223 @@
+(function ($, Drupal) {
+
+  Drupal.behaviors.ama_mainNavigation = {
+    attach: function (context, settings) {
+
+      var $categoryNavWrapper = $('.ama_category_navigation_wrapper'),
+          $categoryNavigationMenu = $('.ama_category_navigation_menu'),
+          $categoryNavigationMenuGroup = $('.ama_category_navigation_menu__group'),
+          $mobileSearchTrigger = $('.global-search-trigger'),
+          $mobileSearch = $('.ama__global-search'),
+          $mainNav = $('.ama__main-navigation '),
+          $productNav = $('.ama__product-nav'),
+          $subMenu = $('.ama_category_navigation_menu__submenu'),
+          $subMenuArticle = $('.ama_category_navigation_menu__articles'),
+          viewportHeight = 0,
+          productNavHeight = 0,
+          categoryNavMenuHeight = $('.ama_category_navigation_menu').outerHeight(),
+          categoryNavMenuResizedHeight = 0,
+          windowWidth = $(window).width();
+
+      // Checks if user agent is a mobile device
+      var deviceAgent = navigator.userAgent.toLowerCase();
+      var agentID = deviceAgent.match(/(android|webos|iphone|ipod|blackberry)/) && windowWidth < 768;
+
+      if($productNav.length && $productNav.is(':visible') ){
+        productNavHeight = $productNav.height();
+      } else {
+        productNavHeight = 0;
+      }
+
+        // Calculate whether or not the category nav should have scrollbars
+      function categoryNavHeight(resizeViewportHeight) {
+
+        // Check to see if viewport height is passed back when the window gets resized
+        if(typeof resizeViewportHeight !== 'undefined') {
+          viewportHeight = resizeViewportHeight;
+        } else {
+          // Window height is used by default
+          viewportHeight = window.innerHeight ? window.innerHeight : $(window).height();
+        }
+
+        // Subtract the navigation height from window height to assess content height
+        categoryNavMenuResizedHeight = viewportHeight;
+        // Check to see if main menu purple dropdown height is larger than viewport height
+        if (categoryNavMenuHeight + $mainNav.outerHeight() + productNavHeight > viewportHeight && !agentID) {
+
+          // Set the menu dropdown the same as viewport to enable scrolling
+          var categoryNavMenuHeightResized = categoryNavMenuResizedHeight - $mainNav.outerHeight() - productNavHeight;
+          $categoryNavigationMenuGroup.addClass('scroll').outerHeight(categoryNavMenuHeightResized);
+
+          $categoryNavigationMenuGroup.on('show.smapi', function(e, menu) {
+            if($('.ama_category_navigation_menu__submenu', menu).outerHeight() > categoryNavMenuHeightResized) {
+              $('.ama_category_navigation_menu__submenu', menu).outerHeight(categoryNavMenuHeightResized);
+            }
+
+            if($('.ama_category_navigation_menu__articles', menu).outerHeight() > categoryNavMenuHeightResized) {
+              $('.ama_category_navigation_menu__articles', menu).outerHeight(categoryNavMenuHeightResized).addClass('one_article');
+            }
+          });
+        } else {
+          $categoryNavigationMenuGroup.removeClass('scroll').outerHeight('auto');
+          $subMenu.outerHeight('auto');
+          $subMenuArticle.outerHeight('auto');
+        }
+      }
+
+
+      // Hide/Show menu
+      function hideShow() {
+        if ($('#global-menu').prop('checked')) {
+          $categoryNavigationMenu.slideDown(function () {
+            if ((categoryNavMenuHeight +  $mainNav.outerHeight() + productNavHeight) > viewportHeight) {
+              bodyScrollLock.disableBodyScroll($categoryNavigationMenuGroup, {
+                allowTouchMove: function allowTouchMove(el) {
+                  while (el && el !== document.body) {
+                    if (el.getAttribute('body-scroll-lock-ignore') !== null) {
+                      return true;
+                    }
+                    el = el.parentNode;
+                  }
+                }
+              });
+            }
+
+            if (agentID) {
+              // Only make the menu height same as viewport on mobile devices
+              var mobileHeight = window.innerHeight ? window.innerHeight : $(window).height();
+              $categoryNavWrapper.height(mobileHeight).addClass('scroll');
+
+              $categoryNavigationMenuGroup.on('show.smapi', function(e, menu) {
+                if($(menu).outerHeight() > mobileHeight) {
+                  $(menu).outerHeight(mobileHeight);
+                }
+
+              });
+            } else {
+              $(this).parent().height('auto');
+              categoryNavHeight();
+            }
+          });
+        }
+        else {
+          $categoryNavigationMenu.slideUp(function () {
+            $(this).parent().height(0);
+            bodyScrollLock.clearAllBodyScrollLocks();
+          });
+        }
+      }
+
+      // Closes menu on doc load
+      $('#global-menu').prop('checked', false);
+
+      $('.ama__global-menu').click(function (e) {
+        hideShow();
+        e.stopPropagation();
+      });
+
+      // If a user clicks outside the menu then close it
+      $(document).click(function (e) {
+        if (!$categoryNavigationMenu.is(e.target) && $categoryNavigationMenu.has(e.target).length === 0) {
+          $('#global-menu').prop('checked', false);
+          hideShow();
+        }
+      });
+
+      $($mobileSearchTrigger).unbind('click').click(function () {
+        $mobileSearch.slideToggle();
+      });
+
+      function moveSocialSharePosition(){
+        var mainNavPosition = $('.ama__main-navigation .container').offset().left;
+        var $amaSocialShare = $('.ama__social-share');
+
+        // Checks to see if there is enough for the sticky nav
+        if(mainNavPosition > 60) {
+
+          var socialStickyPosition = mainNavPosition - 60;
+          var $socialIcons = $('.ama__masthead__content__share');
+
+          // Check to see if viewport width is greater 850px then the social icons will be sticky
+          if($socialIcons.length && $(window).width() > 850) {
+            $socialIcons.sticky({
+              wrapperClassName: 'ama__masthead__content__share-wrapper',
+              zIndex: 501
+            });
+
+            $socialIcons.on('sticky-start', function () {
+              $amaSocialShare.addClass('ama__social-share--fixed').css('left', socialStickyPosition).hide().fadeTo('slow', 1);
+            });
+
+            $socialIcons.on('sticky-update', function () {
+              $amaSocialShare.addClass('ama__social-share--fixed').hide().fadeTo('slow', 1);
+            });
+
+            $socialIcons.on('sticky-end', function () {
+              $('.ama__social-share--fixed').removeClass('ama__social-share--fixed');
+            });
+          }
+        }
+      }
+
+      // Initialize getSocialShare()
+      moveSocialSharePosition();
+
+      // Onscroll check to see if social icon position is greater than footer position
+      var debounce_timer;
+      if($('.ama__masthead__content__share .ama__social-share').is(':visible')) {
+        $(window).scroll(function() {
+          var $socialIcons = $('.ama__masthead__content__share .ama__social-share');
+          var socialIconPositionBottom = $socialIcons.offset().top + $socialIcons.outerHeight();
+          var footerPosition = $('footer').offset().top;
+
+          if(debounce_timer) {
+            window.clearTimeout(debounce_timer);
+          }
+
+          debounce_timer = window.setTimeout(function() {
+            if(socialIconPositionBottom > footerPosition) {
+              $('.ama__masthead__content__share').fadeOut('fast');
+            } else {
+              $('.ama__masthead__content__share').fadeIn('fast');
+            }
+          }, 50);
+        });
+      }
+
+
+      $(window).scroll(function() {
+        var resizeViewportHeight = window.innerHeight ? window.innerHeight : $(window).height();
+        categoryNavHeight(resizeViewportHeight);
+      });
+
+      //Checks the layout position of article on window resize and moves the social icons accordingly
+      $( window ).resize(function() {
+        if (!agentID) {
+          var resizeViewportHeight = $(window).innerHeight();
+          var mainNavPositionUpdate = $('.ama__main-navigation .container').offset().left - 100;
+
+          categoryNavHeight(resizeViewportHeight);
+          $('.ama__social-share.ama__social-share--fixed').css('left', mainNavPositionUpdate);
+        }
+      });
+
+      //If empty or otherwise unpopulated search field (i.e spaces only)
+      //prevent search from submitting and reload current page
+      var searchForm = $("form[id^='block-exposedformacquia-search-solrpage']");
+
+      $(searchForm, this).submit(function(e) {
+          var searchInput = $(this).find("input[name*='search']");
+
+          //Trim and check if search input has any value
+          if ($.trim(searchInput.val()).length < 1) {
+            e.preventDefault();
+            console.log('No search term entered');
+            location.reload();
+          }
+      });
+    }
+  };
+})(jQuery, Drupal);
+
+
+
