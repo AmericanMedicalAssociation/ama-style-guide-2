9823747aa967848afc813096f5bdaa7b42ab71b1
Christine Harder <christine.harder@ama-assn.org>
2022-08-18 15:40:34 +0000

    Update 2022-08-18T15:36:33.221Z

---
+++ assets/js/nav.js
@@ -0,0 +1,52 @@
+/**
+ * @file
+ * Ribbon nav user interactions.
+ *
+ * JavaScript should be made compatible with libraries other than jQuery by
+ * wrapping it with an 'anonymous closure'. See:
+ * - https://drupal.org/node/1446420
+ * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ */
+(function ($, Drupal) {
+
+  Drupal.behaviors.ribbonnav = {
+    attach: function (context, settings) {
+
+      // Needs doc ready because the admin toolbar needs to get loaded to determine the top spacing for sticky nav
+      $(function() {
+        var $bodyFixed = $('body').css('overflow');
+
+        if($bodyFixed === 'hidden') {
+          $('.ama__main-navigation').unstick();
+          return;
+        } else if($(window).width() < 768 ) { // If less than tablet
+          $('.ama__main-navigation').sticky({zIndex: 501});
+        } else if($('.toolbar-tray').hasClass('toolbar-tray-horizontal')) {
+          $('.ama__main-navigation ').sticky({ zIndex: 501, topSpacing: 72 });
+        } else if($('.toolbar-tray').hasClass('toolbar-tray-vertical')) {
+          $('.ama__main-navigation ').sticky({ zIndex: 501, topSpacing: 39 });
+        } else {
+          $('.ama__main-navigation ').sticky({ zIndex: 501 });
+        }
+      });
+
+      $('.ama__ribbon__dropdown').each(function () {
+        var class_active = 'is-active';
+
+        $('.ama__ribbon__dropdown__trigger', this).on('click', function(e) {
+          e.stopPropagation();
+          // Unfocus on the dropdown.
+          $(this).blur();
+          // Add our class for CSS.
+          $(this).toggleClass(class_active);
+          // Add our class to the dropdown UL.
+          $(this).children().toggleClass(class_active);
+        });
+
+        $(document).click( function(){
+          $('.ama__ribbon__dropdown__trigger', this).removeClass(class_active).children().removeClass(class_active);
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);
