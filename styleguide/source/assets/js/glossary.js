275aa7f82238d2e7edf9c380567abe979832f562
joshuathorne <e-blast@joshthorne.com>
2022-09-01 09:54:20 -0500

    attached JS library

--- styleguide/source/assets/js/glossary.js
+++ styleguide/source/assets/js/glossary.js
@@ -1,28 +1,40 @@
-/**
- * @file
- * Initialization script for global processes
- */
-
 (function ($, Drupal) {
+  Drupal.behaviors.autocomplete = {
+    attach: function(context, settings) {
 
-/**
- *
- * Initialize fitVid for YouTube vieos.
- *
- * JavaScript should be made compatible with libraries other than jQuery by
- * wrapping it with an "anonymous closure". See:
- * - https://drupal.org/node/1446420
- * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
- */
+      var $autosuggestionResults = $('ul#ui-id-2');
+      var $displayDiv = $('.autosuggest-wrapper');
+      var $searchInput = $('.searchbar-wrapper input');
+      var $curatedTopics = $('.curated-topics .topic-wrapper-autocomplete');
 
-	Drupal.behaviors.fitvidinit = {
-	 attach: function (context, settings) {
-			(function ($) {
-				$(document).ready(function(){
-					$('.video-container').fitVids();
-				});
-			})(jQuery);
-		}
-	};
+      /* 'ul#ui-id-2' selector is being used because the views autocomplete filter module does not apply
+          identifying classes on the input field or jqueryUI ul autocomplete-element that is shown and hidden.
+          This selector will only work on this particular page. If we build more of these pages, we will need to
+          possibly patch the module. */
 
+
+  $(document).ready(function() {
+    //$autosuggestionResults.append($curatedTopics);
+   // $displayDiv.append($autosuggestionResults);
+    //$displayDiv.append($curatedTopics);
+    //$displayDiv.append($autosuggestionResults);
+/*
+
+    $.widget("custom.autocompletefooter", $.ui.autocomplete, {
+      _renderMenu: function (ul, items) {
+        var self = this;
+        $.each(items, function (index, item) {
+          self._renderItem(ul, item);
+          if (index == items.length - 1) ul.append('<li class="footer-auto"> Footer of autocomplete!!</li>');
+        });
+      }
+    });
+
+*/
+  });
+
+
+
+    }
+  };
 })(jQuery, Drupal);
