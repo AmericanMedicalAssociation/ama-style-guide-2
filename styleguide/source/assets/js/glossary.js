04c9a1fad29fdacc0987d8dad032cb3e7354cff4
joshuathorne <e-blast@joshthorne.com>
2022-09-09 11:41:00 -0500

    initial filter work.

--- styleguide/source/assets/js/glossary.js
+++ styleguide/source/assets/js/glossary.js
@@ -7,13 +7,13 @@
       var $searchInput = $('.searchbar-wrapper input');
       var $curatedTopics = $('.curated-topics .topic-wrapper-autocomplete');
 
-      /* 'ul#ui-id-2' selector is being used because the views autocomplete filter module does not apply
-          identifying classes on the input field or jqueryUI ul autocomplete-element that is shown and hidden.
-          This selector will only work on this particular page. If we build more of these pages, we will need to
-          possibly patch the module. */
 
-
+      //var newURL = location.href.split("?")[0];
+      //window.history.pushState('object', document.title, newURL);
+      //console.log('hiya');
   $(document).ready(function() {
+
+
     //$autosuggestionResults.append($curatedTopics);
    // $displayDiv.append($autosuggestionResults);
     //$displayDiv.append($curatedTopics);
