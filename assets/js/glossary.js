099a7ee07f57daf0e718867ee7cd8f00ce1339aa
joshuathorne <e-blast@joshthorne.com>
2022-09-13 17:10:09 +0000

    Update 2022-09-13T17:09:10.127Z

--- assets/js/glossary.js
+++ assets/js/glossary.js
@@ -1,39 +1,17 @@
 (function ($, Drupal) {
-  Drupal.behaviors.autocomplete = {
+  Drupal.behaviors.glossary_load = {
     attach: function(context, settings) {
 
-      var $autosuggestionResults = $('ul#ui-id-2');
-      var $displayDiv = $('.autosuggest-wrapper');
-      var $searchInput = $('.searchbar-wrapper input');
-      var $curatedTopics = $('.curated-topics .topic-wrapper-autocomplete');
-
-      /* 'ul#ui-id-2' selector is being used because the views autocomplete filter module does not apply
-          identifying classes on the input field or jqueryUI ul autocomplete-element that is shown and hidden.
-          This selector will only work on this particular page. If we build more of these pages, we will need to
-          possibly patch the module. */
-
-
-  $(document).ready(function() {
-    //$autosuggestionResults.append($curatedTopics);
-   // $displayDiv.append($autosuggestionResults);
-    //$displayDiv.append($curatedTopics);
-    //$displayDiv.append($autosuggestionResults);
-/*
-
-    $.widget("custom.autocompletefooter", $.ui.autocomplete, {
-      _renderMenu: function (ul, items) {
-        var self = this;
-        $.each(items, function (index, item) {
-          self._renderItem(ul, item);
-          if (index == items.length - 1) ul.append('<li class="footer-auto"> Footer of autocomplete!!</li>');
-        });
-      }
-    });
-
-*/
-  });
-
-
+      $(document).ready(function() {
+        var regex = /topic=(.*?)&|&topic=.*/i;
+        $('#edit-name--2 a.bef-link').once().each(function (i, link) {
+          var oldUrl = $(link).attr("href");
+          if(regex.test(oldUrl)) {
+            var newUrl = oldUrl.replace(regex, '');
+            $(link).attr("href", newUrl);
+          }
+        })
+      })
 
     }
   };
