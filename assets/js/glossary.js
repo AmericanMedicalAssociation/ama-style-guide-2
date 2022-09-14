f100fd84af720def6e33d8b6b57827d95e3e3eba
joshuathorne <e-blast@joshthorne.com>
2022-09-14 19:24:19 +0000

    Update 2022-09-14T19:22:51.455Z

--- assets/js/glossary.js
+++ assets/js/glossary.js
@@ -6,22 +6,8 @@
 
         var currUrl = window.location.href;
 
-        //  look for any url query.
-        var queryRegex = /\?/i;
-        //  look for an empty name search
-        var emptyRegex = /topic\=$/i;
         //  look for any topic being searched.
         var regex = /topic=(.*?)&|&topic=.*/i;
-
-        //  'All' option should never be selected
-        if($('#edit-name--2 a[name="name[All]"]').hasClass('bef-link--selected')) {
-          $('#edit-name--2 a[name="name[All]"]').removeClass('bef-link--selected');
-        }
-
-        //  Check the url, if there is no active query or a blank search, highlight 'A'.
-        if(!queryRegex.test(currUrl) || emptyRegex.test(currUrl)) {
-          $('#edit-name--2 a[name="name[1]"]').addClass('bef-link--selected');
-        }
 
         //  Loop through the filter links looking for a topic query parameter. If there is one, strip it out.
         $('#edit-name--2 a.bef-link').once().each(function (i, link) {
