233b9df66af4c1a5450d93cc5025af3e06203099
nstriedinger <striedinger@palantir.net>
2022-10-10 13:08:40 -0400

    test autocomplete

---
+++ styleguide/source/assets/js/dr-finder-autocomplete-states.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Autocomplete.
+ *
+ * JavaScript should be made compatible with libraries other than jQuery by
+ * wrapping it with an 'anonymous closure'. See:
+ * - https://drupal.org/node/1446420
+ * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.autoCompleteStates = {
+    attach: function (context, settings) {
+      const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+      $( "#autoCompleteStates" ).autocomplete({
+        source: states
+      });
+    }
+  };
+})(jQuery, Drupal);
