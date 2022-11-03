e35e92b45c3203db295a8369e90f6ef3ee4411c1
Yang Turner <yang@Yangs-MacBook-Pro.local>
2022-11-02 22:39:48 -0600

    Add JS sticky to the Nav bar for profile users and admin role.

--- styleguide/source/assets/js/dr-finder-sticky.js
+++ styleguide/source/assets/js/dr-finder-sticky.js
@@ -5,9 +5,19 @@
  * Since we're using a fraction of AMA JS, we need just this for sticky nav.
  */
 (function ($, Drupal) {
+
   Drupal.behaviors.ama_dr_finder_sticky = {
     attach: function (context, settings) {
-      $('.ama__main-navigation--dr-finder').sticky({zIndex: 501});
+
+      $(function () {
+        // If we logged in as admin, we have to unstick the nav bar for admin role be able to manage the site.
+        if($('body').hasClass('toolbar-tray-open')) {
+          $('.ama__main-navigation--dr-finder').unstick().css("margin-top","8px");
+        }else{
+          $('.ama__main-navigation--dr-finder').sticky();
+        }
+      });
+
     }
   };
 })(jQuery, Drupal);
