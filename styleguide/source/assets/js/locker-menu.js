bb872b9804a985983c53bc86a153c057868407bd
Kevin Graves <kegraves@ama-assn.org>
2023-07-31 12:30:46 -0700

    Fix mobile menu display issues

--- styleguide/source/assets/js/locker-menu.js
+++ styleguide/source/assets/js/locker-menu.js
@@ -5,58 +5,57 @@
 (function ($, Drupal) {
     Drupal.behaviors.ama_lockerMenu = {
       attach: function (context, settings) {
+        // Select required elements from the DOM.
+        const $window = $(window);
+        const $menu = $('.ama_locker_navigation');
+        const $trigger = $('.ama_locker_navigation-trigger');
+        const $catcher = $('.ama_locker_navigation-catcher');
+        const $body = $('body');
+        const bodyFixed = $('body').css('overflow');
 
-        // Select required elements from the DOM
-        const menu = $('.ama_locker_navigation');
-        const trigger = $('.ama_locker_navigation-trigger');
-        const catcher = $('.ama_locker_navigation-catcher');
-        const body = $('body');
-
-        function lockerMenu(menu, trigger, catcher, body) {
-            trigger.once('click-to-show').on('click', function (e) {
-                catcher.toggleClass('hidden').fadeTo('slow', 1);
-                menu.toggleClass('hidden').fadeTo('slow', 1);
-                body.css({"overflow":"hidden"});
+        function lockerMenu() {
+            // Open menu on trigger click.
+            $trigger.once('click-to-show').on('click', function (e) {
+                $menu.toggleClass('hidden');
+                $catcher.toggleClass('hidden');
+                $body.css({"overflow":"hidden"});
             });
-            catcher.once('click-to-hide').on('click', function () {
-                catcher.toggleClass('hidden');
-                menu.fadeToggle( "fast", "linear" );
-                menu.toggleClass('hidden');
-                body.css({"overflow":"auto"});
+            // Close menu on background click.
+            $catcher.once('click-to-hide').on('click', function () {
+                $catcher.toggleClass('hidden');
+                $menu.toggleClass('hidden');
+                $body.css({"overflow":"auto"});
             });
-            // When resizing screen, check size of viewport to update sticky state
-            $( window ).resize(function(){
-                // If less than 850, unstick and remove classes and applied styles from sticky
-                if($(window).width() < 768) {
-                    $('.ama_locker_navigation').unstick();
-                    $('.ama_locker_navigation').toggleClass('hidden');
-                    $('.ama_locker_navigation-trigger').sticky({zIndex: 501, topSpacing: 62});
+            // Update sticky state on window resize.
+            $window.resize(function(){
+                if($window.width() < 600) {
+                    $menu.unstick().addClass('hidden');
+                    $trigger.sticky({zIndex: 501, topSpacing: 62});
                 } else {
-                    $('.ama_locker_navigation-trigger').unstick();
-                    $('.ama_locker_navigation').sticky({zIndex: 501, topSpacing: 60});
+                    $trigger.unstick();
+                    $menu.sticky({zIndex: 501, topSpacing: 60}).removeClass('hidden');
                 }
             });
         }
 
-        // Needs doc ready because the admin toolbar needs to get loaded to determine the top spacing for sticky nav
+        lockerMenu();
+
+        // Need to load admin toolbar before determining top spacing for sticky elements.
         $(function() {
-            var $bodyFixed = $('body').css('overflow');
-            if($bodyFixed === 'hidden') {
+            if(bodyFixed === 'hidden') {
                 $('.ama_locker_navigation').unstick();
             return;
-            } else if($(window).width() < 768 ) { // If less than tablet
-                $('.ama_locker_navigation-trigger').sticky({zIndex: 501, topSpacing: 62});
-                //$('.ama_locker_navigation').toggleClass('hidden');
+            } else if($window.width() < 600) {
+                $menu.unstick().addClass('hidden');
+                $trigger.sticky({zIndex: 501, topSpacing: 62});
             } else if($('.toolbar-tray').hasClass('toolbar-tray-horizontal')) {
-                $('.ama_locker_navigation').sticky({ zIndex: 501, topSpacing: 132 });
+                $menu.sticky({ zIndex: 501, topSpacing: 132 });
             } else if($('.toolbar-tray').hasClass('toolbar-tray-vertical')) {
-                $('.ama_locker_navigation').sticky({ zIndex: 501, topSpacing: 99 });
+                $menu.sticky({ zIndex: 501, topSpacing: 99 });
             } else {
-                $('.ama_locker_navigation').sticky({zIndex: 501, topSpacing: 60});
+                $menu.sticky({zIndex: 501, topSpacing: 60});
             }
         });
-
-        lockerMenu(menu, trigger, catcher, body);
 
       }
     };
