9cc6d40256daea36970369cb504974b50b8c2671
Kevin Graves <kegraves@ama-assn.org>
2023-07-31 01:06:28 -0700

    make css and js updates

--- styleguide/source/assets/js/locker-menu.js
+++ styleguide/source/assets/js/locker-menu.js
@@ -7,29 +7,57 @@
       attach: function (context, settings) {
 
         // Select required elements from the DOM
-        const modal = $('.ama_locker_navigation-menu-mobile');
-        const trigger = $('.locker-menu-trigger');
-        const catcher = $('.click-catcher');
+        const menu = $('.ama_locker_navigation');
+        const trigger = $('.ama_locker_navigation-trigger');
+        const catcher = $('.ama_locker_navigation-catcher');
         const body = $('body');
 
-        function lockerMenu(modal, trigger, catcher, body) {
-          trigger.unbind('click').click(function(e){
-            e.stopPropagation();
-            modal.fadeToggle( "fast", "linear" );
-            body.css({"overflow":"hidden"});
-            modal.toggleClass('hidden');
-
-          });
-          catcher.once('click-to-hide').on('click', function () {
-            modal.fadeToggle( "fast", "linear" );
-            modal.toggleClass('hidden');
-            body.css({"overflow":"auto"});
-          });
+        function lockerMenu(menu, trigger, catcher, body) {
+            trigger.once('click-to-show').on('click', function (e) {
+                catcher.toggleClass('hidden').fadeTo('slow', 1);
+                menu.toggleClass('hidden').fadeTo('slow', 1);
+                body.css({"overflow":"hidden"});
+            });
+            catcher.once('click-to-hide').on('click', function () {
+                catcher.toggleClass('hidden');
+                menu.fadeToggle( "fast", "linear" );
+                menu.toggleClass('hidden');
+                body.css({"overflow":"auto"});
+            });
+            // When resizing screen, check size of viewport to update sticky state
+            $( window ).resize(function(){
+                // If less than 850, unstick and remove classes and applied styles from sticky
+                if($(window).width() < 768) {
+                    $('.ama_locker_navigation').unstick();
+                    $('.ama_locker_navigation').toggleClass('hidden');
+                    $('.ama_locker_navigation-trigger').sticky({zIndex: 501, topSpacing: 62});
+                } else {
+                    $('.ama_locker_navigation-trigger').unstick();
+                    $('.ama_locker_navigation').sticky({zIndex: 501, topSpacing: 60});
+                }
+            });
         }
 
-        lockerMenu(modal, trigger, catcher, body);
+        // Needs doc ready because the admin toolbar needs to get loaded to determine the top spacing for sticky nav
+        $(function() {
+            var $bodyFixed = $('body').css('overflow');
+            if($bodyFixed === 'hidden') {
+                $('.ama_locker_navigation').unstick();
+            return;
+            } else if($(window).width() < 768 ) { // If less than tablet
+                $('.ama_locker_navigation-trigger').sticky({zIndex: 501, topSpacing: 62});
+                //$('.ama_locker_navigation').toggleClass('hidden');
+            } else if($('.toolbar-tray').hasClass('toolbar-tray-horizontal')) {
+                $('.ama_locker_navigation').sticky({ zIndex: 501, topSpacing: 132 });
+            } else if($('.toolbar-tray').hasClass('toolbar-tray-vertical')) {
+                $('.ama_locker_navigation').sticky({ zIndex: 501, topSpacing: 99 });
+            } else {
+                $('.ama_locker_navigation').sticky({zIndex: 501, topSpacing: 60});
+            }
+        });
+
+        lockerMenu(menu, trigger, catcher, body);
 
       }
     };
   })(jQuery, Drupal);
-
