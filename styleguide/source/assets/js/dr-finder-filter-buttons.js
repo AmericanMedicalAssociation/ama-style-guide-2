ad7f3673a31a9cca8e080336a3553da8a2f4d184
Kevin Graves <kegraves@ama-assn.org>
2023-12-08 13:23:10 -0800

    fix drfinder once implementations

--- styleguide/source/assets/js/dr-finder-filter-buttons.js
+++ styleguide/source/assets/js/dr-finder-filter-buttons.js
@@ -7,20 +7,20 @@
  * - https://drupal.org/node/1446420
  * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
  */
-(function ($, Drupal) {
+(function ($, Drupal, once) {
 
   Drupal.behaviors.filterResultsDrFinder = {
-    attach: function (context, settings) {
+    attach: function (context) {
       // All Filters button for mobile.
-      $('.dr-finder-filter-buttons__filters-mobile').once().on('click', function (e) {
+      $(once('mobile-filter', '.dr-finder-filter-buttons__filters-mobile', context)).on('click', function (e) {
         e.stopPropagation();
         $('.dr-finder-filter-list').toggleClass('is-active');
       });
       // X button for mobile.
-      $('.dr-finder-filter-list-mobile-heading svg').once().on('click', function (e) {
+      $(once('mobile-close', '.dr-finder-filter-list-mobile-heading svg', context)).on('click', function (e) {
         e.stopPropagation();
         $('.dr-finder-filter-list').toggleClass('is-active');
       });
     }
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
