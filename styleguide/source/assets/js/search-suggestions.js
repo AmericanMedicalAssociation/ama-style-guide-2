fb9c6aab1faa75f4daadb07dc87a2a9916bfe03b
joshuathorne <e-blast@joshthorne.com>
2024-07-16 17:50:19 -0500

    search suggestions styles.

---
+++ styleguide/source/assets/js/search-suggestions.js
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * Attaches AMA Image Popup library.
+ */
+
+(function ($, Drupal) {
+
+
+
+    Drupal.behaviors.amaSearchSuggestions = {
+        attach: function (context, settings) {
+            $('.ama__global-search input', context).once('amaSearchFocus').on('focus', function () {
+                // Your code to execute when the input is focused goes here
+                console.log('Input focused'); // Example action
+                $('.search-suggestions-wrapper').addClass('show');
+            });
+        }
+    };
+
+    Drupal.behaviors.amaSearchBlur = {
+        attach: function (context, settings) {
+            $('.ama__global-search input', context).once('amaSearchBlur').on('blur', function () {
+                // Your code to execute when the input loses focus goes here
+                console.log('Input lost focus'); // Example action
+                $('.search-suggestions-wrapper').removeClass('show');
+            });
+        }
+    };
+
+})(jQuery, Drupal);
