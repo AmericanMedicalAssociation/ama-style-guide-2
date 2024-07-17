0841fc4f61ab63095adf4cd974b9e3e57ecbe246
joshuathorne <e-blast@joshthorne.com>
2024-07-16 21:28:14 -0500

    scripts

--- styleguide/source/assets/js/search-suggestions.js
+++ styleguide/source/assets/js/search-suggestions.js
@@ -5,11 +5,12 @@
 
 (function ($, Drupal) {
 
-
+console.log('here');
 
     Drupal.behaviors.amaSearchSuggestions = {
         attach: function (context, settings) {
-            $('.ama__global-search input', context).once('amaSearchFocus').on('focus', function () {
+            console.log('Search suggestions attached');
+            $('.ama__global-search form input', context).once('amaSearchFocus').on('focus', function () {
                 // Your code to execute when the input is focused goes here
                 console.log('Input focused'); // Example action
                 $('.search-suggestions-wrapper').addClass('show');
@@ -19,7 +20,7 @@
 
     Drupal.behaviors.amaSearchBlur = {
         attach: function (context, settings) {
-            $('.ama__global-search input', context).once('amaSearchBlur').on('blur', function () {
+            $('.ama__global-search form input', context).once('amaSearchBlur').on('blur', function () {
                 // Your code to execute when the input loses focus goes here
                 console.log('Input lost focus'); // Example action
                 $('.search-suggestions-wrapper').removeClass('show');
