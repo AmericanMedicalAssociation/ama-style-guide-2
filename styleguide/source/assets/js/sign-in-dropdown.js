d88be0c1a92264234288cb24f76dc960e041d2c8
joshuathorne <e-blast@joshthorne.com>
2024-07-26 12:22:32 -0500

    divide event listeners so they don't step on each other.

--- styleguide/source/assets/js/sign-in-dropdown.js
+++ styleguide/source/assets/js/sign-in-dropdown.js
@@ -1,45 +1,52 @@
 (function ($, Drupal) {
-  Drupal.behaviors.ama_signInMenu = {
-    attach: function (context, settings) {
-      var $signInDropdown = $('.ama__sign-in-dropdown');
-      var $signInDropdownMenu = $('.ama__sign-in-dropdown__menu');
-      var $signInLink = $('.ama__sign-in-dropdown__text');
-      var $exploreMenu = $('.ama__explore-menu');
-      var $exploreMenuDropdown = $('.ama__explore-menu__menu');
+    // Define a Drupal behavior named ama_signInMenu
+    Drupal.behaviors.ama_signInMenu = {
+        attach: function (context, settings) {
+            // Select necessary DOM elements
+            var $signInDropdown = $('.ama__sign-in-dropdown');
+            var $signInDropdownMenu = $('.ama__sign-in-dropdown__menu');
+            var $signInLink = $('.ama__sign-in-dropdown__text');
+            var $exploreMenu = $('.ama__explore-menu');
+            var $exploreMenuDropdown = $('.ama__explore-menu__menu');
 
-      function dropdownDownMenu(parentElement, menuElement) {
-       parentElement.unbind('click').click(function(e){
-          e.stopPropagation();
-          $(menuElement).slideToggle();
-          $(parentElement).toggleClass('open');
-        });
+            // Function to handle dropdown menu behavior
+            function dropdownDownMenu(parentElement, menuElement) {
+                // Toggle menu visibility on click
+                parentElement.unbind('click').click(function(e){
+                    e.stopPropagation();
+                    $(menuElement).slideToggle();
+                    $(parentElement).toggleClass('open');
+                });
 
-        // Stop link from firing
-        $signInLink.click(function(e) {
-          e.preventDefault();
-        });
+                // Prevent default action for sign-in link
+                $signInLink.click(function(e) {
+                    e.preventDefault();
+                });
 
-        $(document).click(function(e) {
-          // if the target of the click isn't the container nor a descendant of the container
-          if (!parentElement.is(e.target) && parentElement.has(e.target).length === 0) {
-            $(menuElement).slideUp();
-            $(parentElement).removeClass('open');
-          }
+                // Close menu if click occurs outside of it
+                $(document).click(function(e) {
+                    if (!parentElement.is(e.target) && parentElement.has(e.target).length === 0) {
+                        $(menuElement).slideUp();
+                        $(parentElement).removeClass('open');
+                    }
+                });
 
-          // Set timeout for when a user mouses out of the menu
-          parentElement.mouseenter(function(){
-            clearTimeout();
-          }).mouseleave(function(){
-            setTimeout(function(){
-              $(menuElement).slideUp();
-              $(parentElement).removeClass('open');
-            }, 2000);
-          });
-        });
-      }
+                // Clear timeout on mouse enter
+                parentElement.mouseenter(function() {
+                    clearTimeout(parentElement.data('timeoutId'));
+                }).mouseleave(function() {
+                    // Set timeout to close menu on mouse leave
+                    var timeoutId = setTimeout(function() {
+                        $(menuElement).slideUp();
+                        $(parentElement).removeClass('open');
+                    }, 2000);
+                    parentElement.data('timeoutId', timeoutId);
+                });
+            }
 
-      dropdownDownMenu($signInDropdown, $signInDropdownMenu);
-      dropdownDownMenu($exploreMenu, $exploreMenuDropdown);
-    }
-  };
+            // Attach dropdown behavior to sign-in and explore menus
+            dropdownDownMenu($signInDropdown, $signInDropdownMenu);
+            dropdownDownMenu($exploreMenu, $exploreMenuDropdown);
+        }
+    };
 })(jQuery, Drupal);
