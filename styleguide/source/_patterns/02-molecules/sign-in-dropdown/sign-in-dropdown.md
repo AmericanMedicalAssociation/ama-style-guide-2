3f5f004ec5f5609b4405daa73573d2bc30740824
Joe Mongiat <jmongiat@westmonroe.com>
2024-09-24 17:05:43 -0500

    implement story functionality

--- styleguide/source/_patterns/02-molecules/sign-in-dropdown/sign-in-dropdown.md
+++ styleguide/source/_patterns/02-molecules/sign-in-dropdown/sign-in-dropdown.md
@@ -1,7 +1,13 @@
 ### Description
+
 User dropdown menu with links to user specific pages
 
 [EWL-5597](https://issues.ama-assn.org/browse/EWL-5597)
+
+### Variant options
+
+* [Logged in](?p=molecules-sign-in-dropdown)
+* [Logged out](?p=molecules-sign-in-dropdown~logged-out)
 
 ### Use Case
 
--- styleguide/source/assets/js/sign-in-dropdown.js
+++ styleguide/source/assets/js/sign-in-dropdown.js
@@ -1,54 +1,85 @@
+/**
+ * @file
+ * Attaches behavior to interact with user sign in menu
+ */
 (function ($, Drupal) {
-    Drupal.behaviors.ama_signInMenu = {
-        attach: function (context, settings) {
-            var $signInDropdown = $('.ama__sign-in-dropdown');
-            var $signInDropdownMenu = $('.ama__sign-in-dropdown__menu');
-            var $signInLink = $('.ama__sign-in-dropdown__text');
-            var $exploreMenu = $('.ama__explore-menu');
-            var $exploreMenuDropdown = $('.ama__explore-menu__menu');
-            var isDropdownOpen = false;
-
-            function dropdownDownMenu(parentElement, menuElement) {
-                parentElement.unbind('click').click(function(e){
-                    e.stopPropagation();
-                    if (isDropdownOpen) {
-                        $(menuElement).hide();
-                        $(parentElement).removeClass('open');
-                    } else {
-                        $(menuElement).show();
-                        $(parentElement).addClass('open');
-                    }
-                    isDropdownOpen = !isDropdownOpen;
-                });
-
-                // Stop link from firing
-                $signInLink.click(function(e) {
-                    e.preventDefault();
-                });
-
-                $(document).click(function(e) {
-                    // if the target of the click isn't the container nor a descendant of the container
-                    if (!parentElement.is(e.target) && parentElement.has(e.target).length === 0) {
-                        $(menuElement).hide();
-                        $(parentElement).removeClass('open');
-                        isDropdownOpen = false;
-                    }
-                });
-
-                // Set timeout for when a user mouses out of the menu
-                parentElement.mouseenter(function(){
-                    clearTimeout(parentElement.timeoutId);
-                }).mouseleave(function(){
-                    parentElement.timeoutId = setTimeout(function(){
-                        $(menuElement).hide();
-                        $(parentElement).removeClass('open');
-                        isDropdownOpen = false;
-                    }, 2000);
-                });
+    Drupal.behaviors.amaSignInMenu = {
+      attach: function (context, settings) {
+        const $signInDropdown = $('.ama__sign-in-dropdown__trigger');
+        const $signInDropdownMenu = $('.ama__sign-in-dropdown__menu');
+        const $signInLink = $('.ama__sign-in-dropdown__text');
+        let isDropdownOpen = false;
+  
+        function toggleDropdownMenu(parentElement, menuElement) {
+          parentElement.off('click').on('click', function (e) {
+            e.stopPropagation();
+            if (isDropdownOpen) {
+              closeMenu(menuElement, parentElement);
+            } else {
+              openMenu(menuElement, parentElement);
             }
-
-            dropdownDownMenu($signInDropdown, $signInDropdownMenu);
-            dropdownDownMenu($exploreMenu, $exploreMenuDropdown);
+            isDropdownOpen = !isDropdownOpen;
+          });
+  
+          $signInLink.on('click', function (e) {
+            e.preventDefault();
+          });
+  
+          $(document).on('click', function (e) {
+            if (!parentElement.is(e.target) && parentElement.has(e.target).length === 0) {
+              closeMenu(menuElement, parentElement);
+              isDropdownOpen = false;
+            }
+          });
+  
+          parentElement.on('mouseenter', function () {
+            clearTimeout(parentElement.timeoutId);
+          }).on('mouseleave', function () {
+            parentElement.timeoutId = setTimeout(function () {
+              closeMenu(menuElement, parentElement);
+              isDropdownOpen = false;
+            }, 2000);
+          });
         }
+  
+        function openMenu(menuElement, parentElement) {
+          menuElement.show().addClass('ama__sign-in-dropdown__menu--open');
+          parentElement.addClass('open');
+        }
+  
+        function closeMenu(menuElement, parentElement) {
+          menuElement.hide().removeClass('ama__sign-in-dropdown__menu--open');
+          parentElement.removeClass('open');
+        }
+  
+        toggleDropdownMenu($signInDropdown, $signInDropdownMenu);
+      }
     };
-})(jQuery, Drupal);
+  })(jQuery, Drupal);
+  
+  /**
+   * @file
+   * Attaches behavior to open dismissable sign-in menu when visiting the site
+   */
+  (function ($, Drupal) {
+    Drupal.behaviors.signInCta = {
+      attach: function (context, settings) {
+        const $signInDropdown = $('.ama__sign-in-dropdown__trigger');
+        const $signInDropdownMenu = $('.ama__sign-in-dropdown__menu');
+        const signInCtaCookie = Cookies.get('signInCta');
+  
+        if (signInCtaCookie !== '1') {
+          $signInDropdown.addClass('open');
+          $signInDropdownMenu.addClass('ama__sign-in-dropdown__menu--open');
+  
+          setTimeout(function () {
+            Cookies.set('signInCta', '1', { expires: 1 });
+            $signInDropdownMenu.removeClass('ama__sign-in-dropdown__menu--open');
+          }, 5000);
+        } else {
+          $signInDropdownMenu.removeClass('ama__sign-in-dropdown__menu--open');
+        }
+      }
+    };
+  })(jQuery, Drupal);
+  
