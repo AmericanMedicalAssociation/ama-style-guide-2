dfaf8462b5cc45a8aefdb5f92a16c6334d007e73
Christine Harder <christine.harder@ama-assn.org>
2024-09-26 14:14:09 -0500

    Change mobile product menu height so that the 7th item in the list is always cut off

--- styleguide/source/assets/js/product-menu.js
+++ styleguide/source/assets/js/product-menu.js
@@ -9,9 +9,11 @@
         const $menu = $('.ama__product-nav');
         const $trigger = '.product-menu-title';
         const $catcher = $('.ama__product-nav-catcher');
+        const $menuItems = $('.ama__product-nav .dropdown-container ul li');
         
         $(once('expand-collapse', $trigger, context)).on('click', function (e) {
           if ($menu.hasClass('expanded')) {
+            $('.ama__product-nav.expanded .dropdown-container').css({"max-height": "262px"});
             $menu.removeClass('expanded');
             $catcher.addClass('hidden');
             $('body').css({"overflow":"auto"});
@@ -20,10 +22,18 @@
             $menu.addClass('expanded');
             $catcher.removeClass('hidden');
             $('body').css({"overflow":"hidden"});
+            $menuItems.each(function() {
+              if ($( this ).innerHeight() == '62') {
+                $openDropdown = $('.ama__product-nav.expanded .dropdown-container');
+                $menuHeight = $openDropdown.innerHeight() + 24;
+                $openDropdown.css({"max-height": $menuHeight + "px"});
+              }
+            });
           }
         });
 
         $(once('collapse-product-nav',  $catcher, context)).on('click', function (e) {
+            $('.ama__product-nav.expanded .dropdown-container').css({"max-height": "262px"});
             $menu.removeClass('expanded');
             $catcher.addClass('hidden');
             $('body').css({"overflow":"auto"});
