cb66a575ca1117348cb59ed3ea2a2b8c3e133e3e
joshuathorne <e-blast@joshthorne.com>
2024-11-14 09:45:56 -0600

    supress accordion headings from scrolling on tablet and desktop.

--- styleguide/source/assets/js/anchor_links.js
+++ styleguide/source/assets/js/anchor_links.js
@@ -29,8 +29,8 @@
 
         // On click of any anchor link
         $('a[href^="#"], a[href*="#"]').bind('click', function (e) {
-            // Don't anchor social links. or urls with a /#/ component
-            if (this.getAttribute('data-ga-site_events') == 'social_click' || this.hash.includes('#/')) return;
+            // Don't scroll to anchor social links, urls with a /#/ component, or accordion headings on desktop.
+            if (this.getAttribute('data-ga-site_events') == 'social_click' || this.hash.includes('#/') || (this.tagName.toLowerCase() === 'dt' && $(window).width() > 900)) return;
             e.preventDefault(); // prevent hard jump, the default behavior
           // Perform animated scrolling
           scrollToAnchor(this.hash);
