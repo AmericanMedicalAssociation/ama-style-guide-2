26e8349cda54f5674664168afdeb3b5bbda4d237
joshuathorne <e-blast@joshthorne.com>
2025-01-16 14:26:00 -0600

    remove console logs.

--- styleguide/source/assets/js/anchor_links.js
+++ styleguide/source/assets/js/anchor_links.js
@@ -30,7 +30,7 @@
         // On click of any anchor link
         $('a[href^="#"], a[href*="#"]').bind('click', function (e) {
             // Don't scroll to anchor social links, urls with a /#/ component, or accordion headings on desktop.
-            if (this.getAttribute('data-ga-site_events') == 'social_click' || this.hash.includes('#/') || ($(this).parent().is('dt') && $(window).width() > 900)) return;
+            if (this.getAttribute('data-ga-site_events') == 'social_click' || this.hash.includes('#/') || ($(this).parent().is('dt') && $(window).width() > 992)) return;
             e.preventDefault(); // prevent hard jump, the default behavior
           // Perform animated scrolling
           scrollToAnchor(this.hash);
