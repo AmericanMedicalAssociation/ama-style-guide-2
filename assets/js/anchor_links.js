37ea946f1dca207ad89f4f8d83e53bdbb3047560
Christine Harder <christine.harder@ama-assn.org>
2025-01-23 18:52:32 +0000

    Update 2025-01-23T18:48:38.651Z

--- assets/js/anchor_links.js
+++ assets/js/anchor_links.js
@@ -18,19 +18,19 @@
           var target = $(hash);
           target = target.length ? target : $('[name="' + hash.slice(1) + '"]');
           if (target.length) {
-            $('html, body').animate(
-              {
-                scrollTop: target.offset().top - offset,
-              },
-              500
-            );
+              $('html, body').animate(
+                  {
+                      scrollTop: target.offset().top - offset,
+                  },
+                  500
+                  );
           }
         }
 
         // On click of any anchor link
         $('a[href^="#"], a[href*="#"]').bind('click', function (e) {
             // Don't scroll to anchor social links, urls with a /#/ component, or accordion headings on desktop.
-            if (this.getAttribute('data-ga-site_events') == 'social_click' || this.hash.includes('#/') || (this.tagName.toLowerCase() === 'dt' && $(window).width() > 900)) return;
+            if (this.getAttribute('data-ga-site_events') == 'social_click' || this.hash.includes('#/') || ($(this).parent().is('dt') && $(window).width() > 992)) return;
             e.preventDefault(); // prevent hard jump, the default behavior
           // Perform animated scrolling
           scrollToAnchor(this.hash);
