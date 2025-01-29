50ef1c8240226e6a648eecbddea6d1fef4046c2b
joshuathorne <e-blast@joshthorne.com>
2025-01-28 18:28:53 -0600

    height adjustment.

--- styleguide/source/assets/js/stub-list-height.js
+++ styleguide/source/assets/js/stub-list-height.js
@@ -1,43 +1,45 @@
 (function ($, Drupal) {
     Drupal.behaviors.stubListHeight = {
         attach: function (context, settings) {
-            $('.article-stub-list', context).each(function () {
-                var $articleStubs = $(this).find('.article-stub');
-                var maxHeight = 0;
-                var eyebrows = [];
+            if ($(window).width() > 900) {
+                $('.article-stub-list', context).each(function () {
+                    var $articleStubs = $(this).find('.article-stub');
+                    var maxHeight = 0;
+                    var eyebrows = [];
 
-                // Collect all eyebrow heights and find the maximum height
-                $articleStubs.each(function () {
-                    var $eyebrow = $(this).find('.eyebrow');
-                    if ($eyebrow.length) {
-                        var height = $eyebrow.outerHeight();
-                        eyebrows.push({ $eyebrow: $eyebrow, height: height });
-                        if (height > maxHeight) {
-                            maxHeight = height;
+                    // Collect all eyebrow heights and find the maximum height
+                    $articleStubs.each(function () {
+                        var $eyebrow = $(this).find('.eyebrow');
+                        if ($eyebrow.length) {
+                            var height = $eyebrow.outerHeight();
+                            eyebrows.push({ $eyebrow: $eyebrow, height: height });
+                            if (height > maxHeight) {
+                                maxHeight = height;
+                            }
                         }
+                    });
+
+                    // If no eyebrows are found, do nothing
+                    if (eyebrows.length === 0) {
+                        return;
                     }
-                });
 
-                // If no eyebrows are found, do nothing
-                if (eyebrows.length === 0) {
-                    return;
-                }
+                    // Adjust margin-bottom for images and animate margin-bottom for eyebrows
+                    $articleStubs.each(function () {
+                        var $img = $(this).find('img');
+                        var $eyebrow = $(this).find('.eyebrow');
 
-                // Adjust margin-bottom for images and animate margin-bottom for eyebrows
-                $articleStubs.each(function () {
-                    var $img = $(this).find('img');
-                    var $eyebrow = $(this).find('.eyebrow');
-
-                    if (!$eyebrow.length && $img.css('margin-bottom') === '0px') {
-                        $img.css('margin-bottom', maxHeight + 'px');
-                    } else if ($eyebrow.length) {
-                        var heightDifference = maxHeight - $eyebrow.outerHeight();
-                        if (heightDifference > 0) {
-                            $eyebrow.animate({ 'margin-bottom': (heightDifference + 10) + 'px' }, 200, 'swing');
+                        if (!$eyebrow.length && $img.css('margin-bottom') === '0px') {
+                            $img.css('margin-bottom', maxHeight + 'px');
+                        } else if ($eyebrow.length) {
+                            var heightDifference = maxHeight - $eyebrow.outerHeight();
+                            if (heightDifference > 0) {
+                                $eyebrow.animate({ 'margin-bottom': (heightDifference + 10) + 'px' }, 200, 'swing');
+                            }
                         }
-                    }
+                    });
                 });
-            });
+            }
         }
     };
 })(jQuery, Drupal);
