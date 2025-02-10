2364e16fd6e9c10685b7c8e7ae60a193f941d3ae
joshuathorne <e-blast@joshthorne.com>
2025-02-10 20:38:51 +0000

    Update 2025-02-10T20:29:17.873Z

--- assets/js/anchor_links.js
+++ assets/js/anchor_links.js
@@ -1,46 +1,46 @@
 (function ($, Drupal) {
-  Drupal.behaviors.ama_anchors = {
-    attach: function (context, settings) {
-      $(document).ready(function () {
-        // Function to handle scrolling to anchor
-        function scrollToAnchor(hash) {
-          // Get the height of the header to determine the initial offset
-          var offset = $('header').outerHeight() || 0;
+    Drupal.behaviors.ama_anchors = {
+        attach: function (context, settings) {
+            $(document).ready(function () {
+                // Function to handle scrolling to anchor
+                function scrollToAnchor(hash) {
+                    // Get the height of the header to determine the initial offset
+                    var offset = $('header').outerHeight() || 0;
 
-          // Add the height of the page grouper if it exists
-          if ($(window).width() < 601 && $('.ama_page_grouping_news').length) {
-            offset += $('.ama_page_grouping_news').outerHeight();
-          } else {
-            // Determine the desktop offset based on the presence of the toolbar-horizontal class
-            offset = $('body').hasClass('toolbar-horizontal') ? offset + 80 : offset;
-          }
+                    // Add the height of the page grouper if it exists
+                    if ($(window).width() < 601 && $('.ama_page_grouping_news').length) {
+                        offset += $('.ama_page_grouping_news').outerHeight();
+                    } else {
+                        // Determine the desktop offset based on the presence of the toolbar-horizontal class
+                        offset = $('body').hasClass('toolbar-horizontal') ? offset + 80 : offset;
+                    }
 
-          var target = $(hash);
-          target = target.length ? target : $('[name="' + hash.slice(1) + '"]');
-          if (target.length) {
-              $('html, body').animate(
-                  {
-                      scrollTop: target.offset().top - offset,
-                  },
-                  500
-                  );
-          }
-        }
+                    var target = $(hash);
+                    target = target.length ? target : $('[name="' + hash.slice(1) + '"]');
+                    if (target.length) {
+                        $('html, body').animate(
+                            {
+                                scrollTop: target.offset().top - offset,
+                            },
+                            500
+                        );
+                    }
+                }
 
-        // On click of any anchor link
-        $('a[href^="#"], a[href*="#"]').bind('click', function (e) {
-            // Don't scroll to anchor social links, urls with a /#/ component, or accordion headings on desktop.
-            if (this.getAttribute('data-ga-site_events') == 'social_click' || this.hash.includes('#/') || ($(this).parent().is('dt') && $(window).width() > 992)) return;
-            e.preventDefault(); // prevent hard jump, the default behavior
-          // Perform animated scrolling
-          scrollToAnchor(this.hash);
-        });
+                // On click of any anchor link
+                $('a[href^="#"], a[href*="#"]').bind('click', function (e) {
+                    // Don't scroll to anchor social links, urls with a /#/ component, or accordion headings on desktop.
+                    if (this.getAttribute('data-ga-site_events') == 'social_click' || this.hash.includes('#/') || ($(this).parent().is('dt') && $(window).width() > 992) || /\/comment\/\d+#/.test(this.href)) return;
+                    e.preventDefault(); // prevent hard jump, the default behavior
+                    // Perform animated scrolling
+                    scrollToAnchor(this.hash);
+                });
 
-        // On page load with anchor in URL
-        if (window.location.hash) {
-          scrollToAnchor(window.location.hash);
-        }
-      });
-    },
-  };
+                // On page load with anchor in URL
+                if (window.location.hash) {
+                    scrollToAnchor(window.location.hash);
+                }
+            });
+        },
+    };
 })(jQuery, Drupal);
